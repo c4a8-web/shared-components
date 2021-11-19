ed4350c29a9110c9a554faaff90844f567c263dd
Michael Ehrich <michael.ehrich@krekeny.com>
2021-11-19 09:05:06 +0100

    feat(tools): added getParent

--- assets/js/tools.js
+++ assets/js/tools.js
@@ -11,6 +11,20 @@ class Tools {
       });
     }
   }
+
+  static getParent(element, selector) {
+    const parent = element.parentNode;
+
+    if (parent === null || parent.tagName === "body") {
+      return null;
+    } else {
+      if (!parent.parentNode.querySelector(selector)) {
+        return Tools.getParent(parent, selector);
+      } else {
+        return parent;
+      }
+    }
+  }
 }
 
 export default Tools;
