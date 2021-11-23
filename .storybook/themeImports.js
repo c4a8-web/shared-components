434ac778228bd2918090c50c57df1ca54c6e3154
Michael Ehrich <michael.ehrich@krekeny.com>
2021-11-23 15:52:20 +0100

    feat(themes): added missing themes

--- .storybook/themeImports.js
+++ .storybook/themeImports.js
@@ -1,11 +1,19 @@
 import gkgab from "!!raw-loader!../assets/scss/themes/_gkgab.scss";
 import radiusaas from "!!raw-loader!../assets/scss/themes/_radiusaas.scss";
 import konnekt from "!!raw-loader!../assets/scss/themes/_konnekt.scss";
+import scepman from "!!raw-loader!../assets/scss/themes/_scepman.scss";
+import realmjoin from "!!raw-loader!../assets/scss/themes/_realmjoin.scss";
+import realmigrator from "!!raw-loader!../assets/scss/themes/_realmigrator.scss";
+import unifiedcontacts from "!!raw-loader!../assets/scss/themes/_unified-contacts.scss";
 
 const Styles = {
   gkgab,
   radiusaas,
   konnekt,
+  scepman,
+  realmjoin,
+  realmigrator,
+  unifiedcontacts,
 };
 
 export { Styles };
--- .storybook/themes/src/defaults.js
+++ .storybook/themes/src/defaults.js
@@ -4,11 +4,27 @@ export const THEME_LIST = {
     type: "gkgab",
   },
   radius: {
-    name: "Radius",
+    name: "RADIUSaaS",
     type: "radiusaas",
   },
   konnekt: {
     name: "Konnekt",
     type: "konnekt",
+  },
+  scepman: {
+    name: "SCEPman",
+    type: "scepman",
+  },
+  realmjoin: {
+    name: "RealmJoin",
+    type: "realmjoin",
+  },
+  realmigrator: {
+    name: "RealMigrator",
+    type: "realmigrator",
+  },
+  unifiedcontacts: {
+    name: "UnifiedContacts",
+    type: "unifiedcontacts",
   },
 };
