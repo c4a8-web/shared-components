bb51befcc85cab54ca2cb13965883bb20c32905a
Michael Ehrich <michael.ehrich@krekeny.com>
2021-12-08 12:58:29 +0100

    feat(form): added checkboxes and styling

--- stories/components/form.stories.js
+++ stories/components/form.stories.js
@@ -11,6 +11,8 @@ export default {
 
 const baseArgs = {
   form: {
+    headline: 'Headline Form',
+    subline: 'Subline lorem ipsum dolor sit',
     cta: {
       skin: 'primary',
       width: 'w-50',
@@ -58,6 +60,13 @@ const baseArgs = {
       {
         label: 'Nachricht (optional)',
         type: 'textarea',
+      },
+      {
+        label:
+          "<small>Lorem ipsum dolor sit amet, consectetur adipiscing elit <a href='javascript:void()'>Lorem Ipsum</a>. Sit amet consectetur adipiscing elit.</small>",
+        type: 'checkbox',
+        id: 'privacy',
+        required: true,
       },
     ],
   },
