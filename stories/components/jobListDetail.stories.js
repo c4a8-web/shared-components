998de71d65fce2202d6cb92fe9fe89608da7fd8f
Michael Ehrich <michael.ehrich@krekeny.com>
2021-12-08 12:57:51 +0100

    feat(job-list-detail): added form

--- stories/components/jobListDetail.stories.js
+++ stories/components/jobListDetail.stories.js
@@ -19,6 +19,65 @@ const baseArgs = {
     ctaText: 'Jetzt bewerben',
     ctaButton: true,
   },
+  form: {
+    headline: 'Headline Form',
+    cta: {
+      skin: 'primary',
+      width: 'w-50',
+    },
+    ctaText: 'Bewerbung absenden',
+    method: 'post',
+    action: '',
+    fields: [
+      {
+        label: 'Vorname',
+        type: 'text',
+        col: 6,
+        rowStart: true,
+      },
+      {
+        label: 'Nachname',
+        type: 'text',
+        col: 6,
+        rowEnd: true,
+      },
+      {
+        label: 'E-Mail Adresse',
+        type: 'text',
+        col: 6,
+        rowStart: true,
+      },
+      {
+        label: 'Telefon',
+        type: 'text',
+        col: 6,
+        rowEnd: true,
+      },
+      {
+        label: 'Kündigungsfrist (optional)',
+        type: 'text',
+        col: 6,
+        rowStart: true,
+      },
+      {
+        label: 'Gehaltsvorstellung (optional)',
+        type: 'text',
+        col: 6,
+        rowEnd: true,
+      },
+      {
+        label: 'Nachricht (optional)',
+        type: 'textarea',
+      },
+      {
+        label:
+          "<small>Lorem ipsum dolor sit amet, consectetur adipiscing elit <a href='javascript:void()'>Lorem Ipsum</a>. Sit amet consectetur adipiscing elit.</small>",
+        type: 'checkbox',
+        id: 'privacy',
+        required: true,
+      },
+    ],
+  },
 };
 
 export const Detail = Template.bind({});
