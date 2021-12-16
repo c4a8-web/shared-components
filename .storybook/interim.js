c395ce9698ce53038620d3e3c8a41bb36fcae0ec
Michael Ehrich <michael.ehrich@krekeny.com>
2021-12-16 17:17:29 +0100

    feat(job-list-detail): added stickyness

--- .storybook/interim.js
+++ .storybook/interim.js
@@ -2,6 +2,10 @@ const Interim = () => {
   $('.js-validate').each(function () {
     $.HSCore.components.HSValidation.init($(this));
   });
+
+  $('.js-sticky-block').each(function () {
+    new HSStickyBlock($(this)).init();
+  });
 };
 
 export default Interim;
