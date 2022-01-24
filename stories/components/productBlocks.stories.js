ff88d0709701933d076370fe88c76dc218bc83d3
Michael Ehrich <michael.ehrich@krekeny.com>
2022-01-24 16:15:26 +0100

    feat(product-blocks): migration to storybook

---
+++ stories/components/productBlocks.stories.js
@@ -0,0 +1,53 @@
+import { createComponent, getTitle } from '../../.storybook/templates';
+import { includesproductblockshtml as component } from '../../.storybook/generatedIncludes';
+
+const options = getTitle({
+  title: 'Product Blocks',
+});
+
+export default {
+  ...options,
+};
+
+const Template = (args) => createComponent(args, component);
+
+export const ProductBlocks = Template.bind({});
+
+ProductBlocks.args = {
+  productBlocks: {
+    items: [
+      {
+        image: 'svg/logos/gkgab-SCEPman.svg',
+        headline: '',
+        copy: 'Erklären wer von Kauf über Azur Marketplace profitiert.  dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+        list: [
+          {
+            ctaText: 'Buy Lorem',
+            ctaHref: 'https://www.glueckkanja-gab.com/',
+            target: '_blank',
+          },
+          {
+            ctaText: 'Quote Lorem',
+            ctaHref: 'https://www.glueckkanja-gab.com/',
+            target: '_blank',
+            skin: 'secondary',
+          },
+        ],
+      },
+      {
+        image: 'svg/logos/gkgab-RADIUSaaS.svg',
+        headline: '',
+        copy: 'Erklären wer von Kauf über Azur Marketplace profitiert.  dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. wer von Kauf über Azur Marketplace profitiert.  dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+        list: [
+          {
+            ctaText: 'Buy Lorem',
+            ctaHref: 'https://www.glueckkanja-gab.com/',
+            target: '_blank',
+          },
+        ],
+      },
+    ],
+  },
+  headline: 'Payment options',
+  cloudinary: 'true',
+};
