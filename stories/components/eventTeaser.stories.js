85671d890d65f396c04d3d3f8c816b2ddfa1126a
Michael Ehrich <michael.ehrich@krekeny.com>
2022-02-07 16:59:07 +0100

    feat(event-teaser): added wip version

---
+++ stories/components/eventTeaser.stories.js
@@ -0,0 +1,57 @@
+import { createComponent, getTitle } from '../../.storybook/templates';
+import { includeseventteaserhtml as component } from '../../.storybook/generatedIncludes';
+
+const options = getTitle({
+  title: 'Event Teaser',
+});
+
+export default {
+  ...options,
+};
+
+const Template = (args) => createComponent(args, component);
+
+export const EventTeaserWebcast = Template.bind({});
+
+EventTeaserWebcast.args = {
+  headline: 'Neue Headline 2. Thema, maximal 2 Zeilig',
+  content:
+    '<ul><li>I always had an optimism that was hard for others</li><li>Even in the midst of the all the negative influences from family TWhen I was a young man, even a teenager for that matter, I always had an optimism that was</li></ul>',
+  date: '16.02.2022',
+  time: '14-15 Uhr',
+  shapes: {},
+  authors: 'Mit Nadine Kern & Max Mustermann',
+  image: {
+    img: '',
+    alt: 'lorem ipsum',
+  },
+  badge: {},
+  cta: {
+    text: 'Jetzt anmelden',
+    href: 'javascript:void(0)',
+    skin: 'primary',
+    button: true,
+  },
+  variant: 50,
+};
+
+export const EventTeaserTraining = Template.bind({});
+
+EventTeaserTraining.args = {
+  headline: 'Microsoft 365 Holistic Workplace Security via Teams Meeting',
+  time: '1-4 Tage',
+  shapes: {},
+  authors: 'Mit Nadine Kern & Max Mustermann',
+  image: {
+    img: '',
+    alt: 'lorem ipsum',
+  },
+  badge: {},
+  price: 'Kostenlos',
+  cta: {
+    text: 'Jetzt anmelden',
+    href: 'javascript:void(0)',
+    skin: 'primary',
+    button: true,
+  },
+};
