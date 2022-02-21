1b04822221bd5afe88c8b7df5ba1dd4ded5fd86e
Michael Ehrich <michael.ehrich@krekeny.com>
2022-02-21 16:55:14 +0100

    chore(card): added wip webcast structure

--- stories/components/card.stories.js
+++ stories/components/card.stories.js
@@ -18,7 +18,7 @@ BlogPost.args = {
   layout: 'post',
   title: 'Can Microsoft Teams call 911?',
   date: '2021-05-11',
-  author: 'Christian Kordel',
+  author: ['Christian Kordel', 'Next Author'],
   categories: ['Microsoft', 'Teams', 'Voice', 'SBC', 'Routing'],
   tags: ['Microsoft', 'Teams', 'Voice', 'SBC', 'Routing'],
   blogtitlepic: `${site.blog_image_path}head-emergency-calls`,
@@ -30,15 +30,14 @@ BlogPost.args = {
 export const Webcast = Template.bind({});
 
 Webcast.args = {
-  url: 'javascript:void(0);',
-  layout: 'post',
-  title: 'Can Microsoft Teams call 911?',
-  date: '2021-05-11',
-  author: 'Christian Kordel',
-  categories: ['Microsoft', 'Teams', 'Voice', 'SBC', 'Routing'],
-  tags: ['Microsoft', 'Teams', 'Voice', 'SBC', 'Routing'],
+  title: 'Neue Headline 2. Thema, maximal 2 Zeilig',
+  moment: '2022-02-16',
+  author: ['Nadine Kern', 'Next Author'],
+  webcast: true,
   blogtitlepic: `${site.blog_image_path}head-emergency-calls`,
   socialimg: 'https://res.cloudinary.com/glueckkanja/image/upload/v1620393502/blog/heads/head-emergency-calls.jpg',
+  url: 'javascript:void(0);',
+  tags: ['Microsoft', 'Teams', 'Voice', 'SBC', 'Routing'],
   excerpt:
     'In allen Teams Voice Projekten kommt man mit dem Kunden an den Punkt, dass man über ein Konzept für Notrufe sprechen muss. Unsere generelle Empfehlung ist, die Mitarbeiter anzuweisen, Notrufe von ihrem Handy aus zu tätigen. Dies ist jedoch nicht bei allen Kunden möglich. Daher muss auch dieses Szenario mit den Möglichkeiten der Teams Voice Plattform abgedeckt werden.',
 };
