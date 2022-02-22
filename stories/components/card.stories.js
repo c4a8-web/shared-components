3e279eee4316b34f97b6030bfe42105a3bde94b5
Michael Ehrich <michael.ehrich@krekeny.com>
2022-02-22 11:09:18 +0100

    fix(card): max-width issue

--- stories/components/card.stories.js
+++ stories/components/card.stories.js
@@ -27,6 +27,23 @@ BlogPost.args = {
     'In allen Teams Voice Projekten kommt man mit dem Kunden an den Punkt, dass man über ein Konzept für Notrufe sprechen muss. Unsere generelle Empfehlung ist, die Mitarbeiter anzuweisen, Notrufe von ihrem Handy aus zu tätigen. Dies ist jedoch nicht bei allen Kunden möglich. Daher muss auch dieses Szenario mit den Möglichkeiten der Teams Voice Plattform abgedeckt werden.',
 };
 
+export const BlogPostLarge = Template.bind({});
+
+BlogPostLarge.args = {
+  large: true,
+  url: 'javascript:void(0);',
+  layout: 'post',
+  title: 'Can Microsoft Teams call 911?',
+  date: '2021-05-11',
+  author: ['Christian Kordel', 'Next Author'],
+  categories: ['Microsoft', 'Teams', 'Voice', 'SBC', 'Routing'],
+  tags: ['Microsoft', 'Teams', 'Voice', 'SBC', 'Routing'],
+  blogtitlepic: `${site.blog_image_path}head-emergency-calls`,
+  socialimg: 'https://res.cloudinary.com/glueckkanja/image/upload/v1620393502/blog/heads/head-emergency-calls.jpg',
+  excerpt:
+    'In allen Teams Voice Projekten kommt man mit dem Kunden an den Punkt, dass man über ein Konzept für Notrufe sprechen muss. Unsere generelle Empfehlung ist, die Mitarbeiter anzuweisen, Notrufe von ihrem Handy aus zu tätigen. Dies ist jedoch nicht bei allen Kunden möglich. Daher muss auch dieses Szenario mit den Möglichkeiten der Teams Voice Plattform abgedeckt werden.',
+};
+
 export const Webcast = Template.bind({});
 
 Webcast.args = {
