d880d28f0be7b23e13cc0ff9be0ff2819f798e15
Michael Ehrich <michael.ehrich@krekeny.com>
2022-02-23 11:41:01 +0100

    feat(authors): added no link variant

---
+++ stories/components/authors.stories.js
@@ -0,0 +1,25 @@
+import { createComponent, getTitle } from '../../.storybook/templates';
+import { includesauthorshtml as component } from '../../.storybook/generatedIncludes';
+
+const options = getTitle({
+  title: 'Authors',
+});
+
+export default {
+  ...options,
+};
+
+const Template = (args) => createComponent(args, component);
+
+export const WithLink = Template.bind({});
+
+WithLink.args = {
+  authors: ['Author Name', 'Second Author'],
+};
+
+export const WithoutLink = Template.bind({});
+
+WithoutLink.args = {
+  authors: ['Author Name', 'Second Author'],
+  noLink: true,
+};
