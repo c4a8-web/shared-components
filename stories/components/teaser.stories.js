c28286c6aa47291a0dcd94cc964e2f889e8505b0
Michael Ehrich <michael.ehrich@krekeny.com>
2022-04-14 14:50:58 +0200

    fix(teaser): no video variant fixed

--- stories/components/teaser.stories.js
+++ stories/components/teaser.stories.js
@@ -27,3 +27,19 @@ Teaser.args = {
   logoAlt: 'alt logo',
   text: 'Effiziente Umsetzung der DEKRA Cloud First Journey mit DevOps und Infrastructure as Code (IaC)',
 };
+
+export const TeaserNoVideo = Template.bind({});
+
+TeaserNoVideo.args = {
+  ctaText: 'Case Study ansehen',
+  ctaHref: '',
+  type: 'button',
+  target: '_blank',
+  thumb: 'shared-components/teaser-dekra.png',
+  alt: 'Video Casestudy Uniper',
+  headline: 'Azure @ DEKRA',
+  overline: 'Zufriedene Kunden',
+  logo: 'h_40,w_140/shared-components/teaser-dekra-logo.png',
+  logoAlt: 'alt logo',
+  text: 'Effiziente Umsetzung der DEKRA Cloud First Journey mit DevOps und Infrastructure as Code (IaC)',
+};
