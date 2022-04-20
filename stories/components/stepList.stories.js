26ca90836bf8826b4d78492943c4396aa00801e9
Michael Ehrich <michael.ehrich@krekeny.com>
2022-04-20 17:28:56 +0200

    feat(step-list): added small variant

--- stories/components/stepList.stories.js
+++ stories/components/stepList.stories.js
@@ -11,9 +11,9 @@ export default {
 
 const Template = (args) => createComponent(args, component);
 
-export const StepList = Template.bind({});
+export const Default = Template.bind({});
 
-StepList.args = {
+Default.args = {
   headline: {
     text: 'Unser Bewerbungsprozess',
     level: 'h3',
@@ -35,6 +35,36 @@ StepList.args = {
       title: 'Lorem ipsum dolor sit',
       content:
         'Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und Konsonantien leben die Blindtexte. Abgeschieden wohnen sie',
+    },
+  ],
+};
+
+export const Small = Template.bind({});
+
+// TODO manual override step count
+
+Small.args = {
+  variant: 'small',
+  headline: {
+    text: 'Experiencing & Pilot Package',
+    level: 'h2',
+  },
+  list: [
+    {
+      title: 'Envisioning & Experience Workshop',
+      content:
+        '<ul class="dashed"> <li>AVD Architektur</li> <li>Roadmap Implementierung</li> <li>Lizenzierung</li> <li>Bewertung Benutzer & Apps (limitiert auf 3-5 Apps)</li> <li>Prod. Anforderungen & Governance</li> <li>Überprüfung des aktuellen Azure Footprint</li> <li><strong><span class="highlight-yellow">Konzept Po</span>werPoint</strong></li> </ul>',
+    },
+    {
+      title: 'Pilot Implementaion',
+      content:
+        '<ul class="dashed"> <li>Pilot AVD Implementierung</li> <li>Manuelle Einrichtung eines Multisession Images mit 3 Geschäftsanwendungen</li> <li>Setup auf Basis von Best Practices für AVD Security & Networking</li> <li>Standard AVD Policies (GPOs)</li> <li>Automatische AVD Herunterskalierung</li> <li><strong>Ideal zum Te<span class="highlight-yellow">sten von Erfahrun</span>gen und Leistungen</strong></li> </ul>',
+    },
+    {
+      step: 99,
+      title: 'Roadmap to Production',
+      content:
+        '<ul class="dashed"> <li><strong>Ro<span class="highlight-yellow">admap zum produktiven</span> Einsatz</strong></li> <li>Präsentation der Pilot- und Testergebnisse</li> <li>Überprüfung zusätzlicher Geschäftsanwendungen</li> <li>Prozess Diskussion zu:</li> <ul class="dashed"> <li>Updates (OS & Apps)</li> <li>Monitoring</li> <li>Printing</li> </ul> </ul>',
     },
   ],
 };
