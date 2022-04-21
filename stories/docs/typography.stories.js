5422cc63568128eb053c7682a2fb0011b680a779
Michael Ehrich <michael.ehrich@krekeny.com>
2022-04-21 15:48:52 +0200

    feat(docs): added tooltip for typo sizes

--- stories/docs/typography.stories.js
+++ stories/docs/typography.stories.js
@@ -1,6 +1,7 @@
 import { createComponent, getTitle } from '../../.storybook/templates';
 
 import typographyHtml from '!!raw-loader!./typography.html';
+import styles from '../../assets/scss/_exports.scss';
 
 const options = getTitle({
   title: 'Typography',
@@ -17,100 +18,118 @@ const baseArgs = {};
 
 export const Typography = Template.bind({});
 
+const settings = {
+  'Headline Sizes': null,
+  'h1-font-size': {
+    breakpoints: ['sm', 'lg', ''],
+    variant: 'light',
+  },
+  'h2-font-size': {
+    breakpoints: ['sm', 'lg', ''],
+    variant: 'bold',
+  },
+  'h3-font-size': {
+    breakpoints: ['sm', 'lg', ''],
+    variant: 'bold',
+  },
+  'h4-font-size': {
+    breakpoints: ['sm', 'lg', ''],
+  },
+  'h5-font-size': {
+    breakpoints: ['sm', 'lg', ''],
+  },
+  'h6-font-size': {
+    breakpoints: ['sm', 'lg', ''],
+  },
+  'Copy Sizes': null,
+  'font-size-1': {
+    breakpoints: ['sm', 'lg', ''],
+    variant: 'bold',
+  },
+  'font-size-2': {
+    breakpoints: ['sm', 'lg', ''],
+    variant: 'bold',
+  },
+  'font-size-3': {
+    breakpoints: ['sm', 'lg', ''],
+    variant: 'bold',
+  },
+  'font-size-4': {
+    breakpoints: ['sm', 'lg', ''],
+    variant: 'light',
+  },
+  'Extra Sizes': null,
+  'font-size-5': {
+    breakpoints: ['sm', 'lg', ''],
+    variant: 'light',
+  },
+  'font-size-6': {
+    breakpoints: ['sm', 'lg', ''],
+    variant: 'light',
+  },
+  'font-size-7': {
+    breakpoints: ['sm', 'lg', ''],
+    variant: 'light',
+  },
+};
+
+const baseFontSize = 16;
+const generatedTypes = [];
+const formatter = new Intl.NumberFormat('en-EN', {
+  minimumFractionDigits: 0,
+  maximumFractionDigits: 2,
+});
+
+const generateDocs = function (typeFace, breakpoints) {
+  if (!breakpoints) return;
+
+  let docs = '';
+
+  breakpoints.forEach(function (breakpoint) {
+    const typeFaceKey = `${typeFace}${breakpoint ? '-' + breakpoint : ''}`;
+    const typeFaceValue = styles[typeFaceKey];
+
+    if (!typeFaceValue) return;
+
+    const pxValue = typeFaceValue.replace('rem', '') * baseFontSize;
+    const formattedValue = formatter.format(pxValue);
+
+    docs += `${breakpoint ? 'Breakpoint (' + breakpoint + ')' : 'Above Breakpoint'}: ${formattedValue}px<br/>`;
+  });
+
+  if (docs === '') return null;
+
+  return docs;
+};
+
+[].forEach.call(Object.keys(settings), (typeFace) => {
+  const typeFaceSettings = settings[typeFace];
+
+  if (!typeFaceSettings)
+    return generatedTypes.push({
+      headline: typeFace,
+    });
+
+  const docs = generateDocs(typeFace, typeFaceSettings?.breakpoints);
+
+  const typeFaceDefault = {
+    class: typeFace,
+    docs,
+  };
+
+  generatedTypes.push(typeFaceDefault);
+
+  if (typeFaceSettings.variant) {
+    const typeFaceVariant = {
+      ...typeFaceDefault,
+    };
+
+    typeFaceVariant[typeFaceSettings.variant] = true;
+    generatedTypes.push(typeFaceVariant);
+  }
+});
+
 Typography.args = {
   ...baseArgs,
-  types: [
-    {
-      headline: 'Headline Sizes',
-    },
-    {
-      class: 'h1-font-size',
-      light: true,
-    },
-    {
-      class: 'h1-font-size',
-    },
-    {
-      class: 'h2-font-size',
-    },
-    {
-      class: 'h2-font-size',
-      bold: true,
-    },
-    {
-      class: 'h3-font-size',
-    },
-    {
-      class: 'h3-font-size',
-      bold: true,
-    },
-    {
-      class: 'h4-font-size',
-    },
-    {
-      class: 'h4-font-size',
-      light: true,
-    },
-    {
-      class: 'h5-font-size',
-    },
-    {
-      class: 'h6-font-size',
-    },
-    {
-      headline: 'Copy Sizes',
-    },
-    {
-      class: 'font-size-1',
-    },
-    {
-      class: 'font-size-1',
-      bold: true,
-    },
-    {
-      class: 'font-size-2',
-    },
-    {
-      class: 'font-size-2',
-      bold: true,
-    },
-    {
-      class: 'font-size-3',
-    },
-    {
-      class: 'font-size-3',
-      bold: true,
-    },
-    {
-      class: 'font-size-4',
-    },
-    {
-      class: 'font-size-4',
-      light: true,
-    },
-    {
-      headline: 'Extra Sizes',
-    },
-    {
-      class: 'font-size-5',
-    },
-    {
-      class: 'font-size-5',
-      light: true,
-    },
-    {
-      class: 'font-size-6',
-    },
-    {
-      class: 'font-size-6',
-      light: true,
-    },
-    {
-      class: 'font-size-7',
-    },
-    {
-      class: 'font-size-7',
-      light: true,
-    },
-  ],
+  types: [...generatedTypes],
 };
