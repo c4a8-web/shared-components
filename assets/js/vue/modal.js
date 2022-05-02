4a98523a45820fd6140ed1175f457a029324f30c
Michael Ehrich <michael.ehrich@krekeny.com>
2022-05-02 13:44:39 +0200

    feat(modal): migrated to a vue component

--- assets/js/vue/modal.js
+++ assets/js/vue/modal.js
@@ -2,41 +2,24 @@ export default {
   tagName: 'modal',
   computed: {
     classList() {
-      return [
-        'modal fade',
-        // `icon--${this.direction}`,
-        // `icon--${this.icon}`,
-        // `${this.animation ? 'js-text-animation__icon' : ''}`,
-        // `${this.circle ? 'icon--circle' : ''}`,
-        // `${this.hover ? 'icon--hover' : ''}`,
-        'vue-component',
-      ];
+      return ['modal fade', 'vue-component'];
     },
-    // settings() {
-    //   // {% if modalClientName %}data-client-name="{{ modalClientName }}" {% endif %}
-    //   // {% if modalApiUrl %}data-api-url="{{ modalApiUrl }}" {% endif %}
-    //   // {% if modalJobId %}data-job-id="{{ modalJobId }}" {% endif %}
-
-    //   return {
-    //     'data-client-name': this.clientName ? this.clientName : null,
-    //     'data-api-url': this.apiUrl ? this.apiUrl : null,
-    //     'data-job-id': this.jobId ? this.jobId : null,
-    //   };
-    // },
+    settings() {
+      return {
+        'data-client-name': this.clientName ? this.clientName : null,
+        'data-api-url': this.apiUrl ? this.apiUrl : null,
+        'data-job-id': this.jobId ? this.jobId : null,
+      };
+    },
   },
   props: {
-    form: Object,
-    success: Object,
-    application: {
-      default: false,
-    },
     clientName: String,
     apiUrl: String,
     jobId: String,
   },
   template: `
     <div :class="classList" tabindex="-1" aria-hidden="true" style="--color-icon-hover-color: var(--color-white)"
-          :data-client-name="clientName ? clientName : null">
+          v-bind="settings" >
       <div class="modal-dialog modal-xl">
         <div class="modal__content">
           <div class="modal__header">
@@ -45,14 +28,7 @@ export default {
             </div>
           </div>
           <div class="modal__body">
-          modal modal
-            {% if modalApplication %}
-              {%
-                include modal-application.html
-                form=modalForm
-                success=modalSuccess
-              %}
-            {% endif %}
+            <slot></slot>
           </div>
         </div>
       </div>
