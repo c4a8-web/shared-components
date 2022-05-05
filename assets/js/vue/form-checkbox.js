ec2e6dd01a3be377bebfe0668527e0f5838971a6
Michael Ehrich <michael.ehrich@krekeny.com>
2022-05-05 18:56:07 +0200

    fix(form-checkbox): id corrected

--- assets/js/vue/form-checkbox.js
+++ assets/js/vue/form-checkbox.js
@@ -7,6 +7,9 @@ export default {
     required() {
       return this.checkbox?.required ? 'required' : null;
     },
+    idValue() {
+      return this.id ? this.id : this.checkbox.id;
+    },
   },
   props: {
     checkbox: Object,
@@ -18,13 +21,13 @@ export default {
       <template v-if="checkbox?.placeholder">
         <input class="form__checkbox form-check-input custom-control-input" type="checkbox" :data-form-group="group" :required="required" :name="otherId" :id="otherId">
         <label class="form__checkbox-label custom-control-label" :for="otherId"></label>
-        <input class="form__input form-control form-control-sm" type="text" :name="checkbox?.id" :id="checkbox?.id" :placeholder="checkbox?.placeholder" :data-form-group="group">
+        <input class="form__input form-control form-control-sm" type="text" :name="idValue" :id="idValue" :placeholder="checkbox?.placeholder" :data-form-group="group">
       </template>
       <template v-else>
-        <input class="form__checkbox form-check-input custom-control-input" type="checkbox" :name="checkbox?.id" :id="checkbox?.id" :data-form-group="group" :required="required">
-        <label class="form__checkbox-label custom-control-label" :for="checkbox?.id" v-html="checkbox?.label">
+        <input class="form__checkbox form-check-input custom-control-input" type="checkbox" :name="idValue" :id="idValue" :data-form-group="group" :required="required">
+        <label class="form__checkbox-label custom-control-label" :for="idValue" v-html="checkbox?.label">
         </label>
       </template>
     </div>
-    `,
+  `,
 };
