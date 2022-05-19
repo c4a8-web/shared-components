e256ed9000399e6ba522fd12cf4fb075742d286d
Adnan <adnan.ahmed@glueckkanja-gab.com>
2022-05-19 20:00:53 +0200

    tag cloud feature

--- assets/js/components/tag-cloud.js
+++ assets/js/components/tag-cloud.js
@@ -9,6 +9,7 @@ class TagCloud extends BaseComponent {
 
     this.tagSelector = '.tag-cloud__tag';
 
+    // tag cloud already existant?
     this.builded = false;
     //radius of the sphere
     this.radius = 200;
@@ -16,12 +17,12 @@ class TagCloud extends BaseComponent {
     this.radiansTranslate = Math.Pi/180;
     // distance
     this.distance = 300;
-    // list
+    // list with text elements
     this.mclist = [];
     // is mouse currently over the sphere
     // delete/replace later
     this.active = false;
-
+    // last a b values
     this.lasta = 1;
     this.lastb = 1;
     //equally distributed points on a sphere
@@ -29,12 +30,12 @@ class TagCloud extends BaseComponent {
     //rotation velocity
     this.velocity = 5;
     this.size = 300;
-
+    //TODO: Remove
     this.mouseX = 0;
     this.mouseY = 0;
-
+    // type of movement
     this.howElliptical = 1;
-
+    // text elements
     this.aA = null;
     this.oDiv = null;
     this.req = null;
@@ -42,7 +43,7 @@ class TagCloud extends BaseComponent {
     this.bindEvents();
   }
 
-
+  // turnon and off functions
   turnOn(target, event, option, func){
     let tg = (target != undefined) ? target : window;
     if (window.addEventListener) {
@@ -61,20 +62,158 @@ class TagCloud extends BaseComponent {
     }
   }
 
-  hasBeenBuilded() {
+  // assigning sinus and cosinus to values
+  sineCosine(a, b, c) {
+    sinusa = Math.sin(a * this.radiansTranslate);
+    cosinusa = Math.cos(a * this.radiansTranslate);
+    sinusb = Math.sin(b * this.radiansTranslate);
+    cosinub = Math.cos(b * this.radiansTranslate);
+    sinusc = Math.sin(c * this.radiansTranslate);
+    cosinusc = Math.cos(c * this.radiansTranslate);
+  }
+
+  // build and destroy
+  tagCloudBuild() {
     if (this.builded) return;
-
     this.builded = true;
-  }
+
+    let i = 0;
+    let oTag = null;
+
+    // element selection
+    oDiv = document.querySelector('#div1');
+    aA = oDiv.querySelector(".text");
 
 
-  bindEvents() {
-    this.root.addEventListener('click', this.handleClick.bind(this));
+    for (i = 0; i < aA.length; i++) {
+      oTag = aA[i];
+      oTag.offsetWidth = aA[i].offsetWidth;
+      oTag.offsetHeight = aA[i].offsetHeight;
+
+      this.mclist.push(oTag);
+    }
+
+    sineCosine( 0, 0, 0 );
+
+    positionAll();
+
+    //TODO: add event
+
+    update();
   }
 
-  handleClick() {
-    Tools.scrollToTop();
+  tagCloudDestroy() {
+    //TODO: add event
+    cancelAnimationFrame(this.req);
+    this.mclist = [];
+    this.builded = false;
   }
+
+  // updates the position of the elements
+  update() {
+    let a;
+    let b;
+
+    // get a b values
+    if (this.active) {
+      //TODO: replace this.mouseY with diff Event
+      a = (-Math.min(Math.max(-this.mouseY, -this.size), this.size) / this.size) * this.velocity;
+      b = (Math.min(Math.max(-this.mouseX, -this.size), this.size) / this.size) * this.velocity;
+    } else {
+      a = this.lasta * 0.98;
+      b = this.lastb * 0.98;
+    }
+
+    // save old a b values
+    this.lasta = a;
+    this.lastb = b;
+
+    this.req = window.requestAnimationFrame(this.update);
+
+    // check absolute value of a b
+    if (Math.abs(a) <= 0.01 && Math.abs(b) <= 0.01) {
+      return;
+    }
+
+    // get sinus and cosinus for a b c
+    let c = 0;
+    sineCosine(a, b, c);
+    for (let j = 0; j < this.mclist.length; j++) {
+      /* Rotationmatrix Transformations for positioning/rotation of Text Elements */
+
+      let rx1 = this.mclist[j].cx;
+      let ry1 = this.mclist[j].cy * cosinusa + this.mclist[j].cz * (-sinusa);
+      let rz1 = this.mclist[j].cy * sinusa + this.mclist[j].cz * ccosinusa;
+
+      let rx2 = rx1 * cosinusb + rz1 * sinusb;
+      let ry2 = ry1;
+      let rz2 = rx1 * (-sinusb) + rz1 * cosinusb;
+
+      let rx3 = rx2 * cosinusc + ry2 * sinususc;
+      let ry3 = rx2 * (-sinususc) + ry2 * cosinusc;
+      let rz3 = rz2;
+
+      this.mclist[j].cx = rx3;
+      this.mclist[j].cy = ry3;
+      this.mclist[j].cz = rz3;
+
+      // distance from the center of the sphere
+      per = this.distance / (this.distance + rz3);
+
+      this.mcList[j].x = (this.howElliptical * rx3 * per) - (this.howElliptical * 2);
+      this.mcList[j].y = ry3 * per;
+
+      // scale based on the distance from the sphere
+      this.mcList[j].scale = per;
+
+      // opacity based on the distance from the sphere
+      this.mcList[j].alpha = per;
+      this.mcList[j].alpha = (per - 0.6) * (3 * per - 1.6);
+    }
+
+    doPosition();
+    depthSort();
+
+  }
+
+  // keeps the elements in the right position by checking their z value
+  depthSort() {
+    let i = 0;
+    let aTmp = [];
+
+    for (i = 0; i < this.aA.length; i++) {
+      aTmp.push(this.aA[i]);
+    }
+
+    aTmp.sort(function(a, b) {
+      if (a.cz > b.cz) {
+        return -1;
+      } else if (a.cz < b.cz) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
+    for (i = 0; i < aTmp.length; i++) {
+      aTmp[i].style.zIndex = i;
+    }
+  }
+
+  positionAll() {
+    let phi = 0;
+    let theta = 0;
+    let max = this.mclist.length;
+    let i = 0;
+
+    let aTmp = [];
+    let oFragment = document.createDocumentFragment();
+
+    for (i = 0; i < this.aA.length; i++) {
+      aTmp.push(this.aA[i]);
+    }
+  }
+
 }
 
 export default TagCloud;
