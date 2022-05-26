a3908450e150c0f1148c59c84f3120a4808bb902
Adnan <adnan.ahmed@glueckkanja-gab.com>
2022-05-26 13:01:16 +0200

    feat(tag-cloud): displays Elements in a sphere (rotation missing)

--- assets/js/components/tag-cloud.js
+++ assets/js/components/tag-cloud.js
@@ -11,11 +11,56 @@ class TagCloud extends BaseComponent {
     // a list with all our ObjectTags for each textElement
     this.tagList = []
     this.depth = 300;
-    this.radius = 300;
-    this.update(20, 20, 20);
+    this.radius = 150;
+
+    //mouse position
+    this.mouseX = 0;
+    this.mouseY = 0;
+    this.mouseIsOver = false;
+    this.velocity = 5;
+
+    this.update(10, 5, 20);
   }
 
+  // replace mouse by random input
+  //
 
+
+  //mouse tracking of current state
+  mouseOver() {
+    this.mouseIsOver = true;
+  }
+
+  mouseOut() {
+    this.mouseIsOver = false;
+  }
+
+  mouseMove() {
+    let oEvent = window.event || ev;
+    this.mouseX = oEvent.clientX
+    console.log(this.mouseX);
+  }
+
+  //evaluate the mouse position and update the positions
+  mouseEvaluation() {
+    this.divObject.addEventListener('mouseover', this.mouseOver());
+    this.divObject.addEventListener('mouseout', this.mouseOut());
+    this.divObject.addEventListener('mousemove', this.mouseMove());
+
+    let size = 200;
+    if (this.mouseIsOver) {
+      //
+      this.mouseY = -(Math.min(Math.max(-this.mouseY, -size)/this.radius) * this.velocity);
+      this.mouseX = (Math.min(Math.max(-this.mouseX, -size), size)/this.radius) * this.velocity;
+      this.lastY = this.mouseY;
+      this.lastX = this.mouseX;
+    } else {
+      this.mouseY = this.lastY * 0.98;
+      this.mouseX = this.lastX * 0.98;
+    }
+
+    this.update(this.mouseX, this.mouseY, 0);
+  }
 
   setTags() {
     for (let i = 0; i < this.textElements.length; i++) {
@@ -27,14 +72,17 @@ class TagCloud extends BaseComponent {
     }
   }
 
+
   positionObjects() {
     let phi = 0;
     let theta = 0;
     let i = 0
     let max = this.tagList.length + 1;
     for (i = 1; i < max; i++) {
+      if (true) {}
       phi = Math.acos(-1 + (2 * i + 1 )/max);
       theta = Math.sqrt(max * Math.PI) * phi;
+
       this.tagList[i -1].cx = this.radius * Math.cos(theta) * Math.sin(phi);
       this.tagList[i -1].cy = this.radius * Math.sin(theta) * Math.sin(phi);
       this.tagList[i -1].cz = this.radius * Math.cos(phi);
@@ -56,18 +104,18 @@ class TagCloud extends BaseComponent {
   rotationMatrix(a, b, c, i) {
     let result = this.sinCos(a, b, c);
     let rx1 = this.tagList[i].cx;
-    let ry1 = this.tagList[i].cy * result.cosA - this.tagList[i].cy * (-result.sinA);
-    let rz1 = this.tagList[i].cy * result.sinA + this.tagList[i].cy * result.cosA;
+    let ry1 = this.tagList[i].cy * result.cosA + this.tagList[i].cz * (-result.sinA);
+    let rz1 = this.tagList[i].cy * result.sinA + this.tagList[i].cz * result.cosA;
 
-    let rx2 = rx1 * result.cosB + rz1 * (+result.sinB);
+    let rx2 = rx1 * result.cosB + rz1 * result.sinB;
     let ry2 = ry1;
     let rz2 = rx1 * (-result.sinB) + rz1 * result.cosB;
 
-    let rx3 = rx2 * result.cosC + ry2 * (+result.sinC);
+    let rx3 = rx2 * result.cosC + ry2 * (-result.sinC);
     let ry3 = rx2 * result.sinC + ry2 * result.cosC;
     let rz3 = rz2;
 
-    let per = this.depth / this.depth + rz3
+    let per = this.depth / (this.depth + rz3);
     return { rx3, ry3, rz3, per };
     }
 
@@ -81,21 +129,30 @@ class TagCloud extends BaseComponent {
       this.tagList[i].cx = vector3.rx3;
       this.tagList[i].cy = vector3.ry3;
       this.tagList[i].cz = vector3.rz3;
-
+      console.log(vector3.per);
       this.tagList[i].x = (vector3.rx3 * vector3.per) - 2;
       this.tagList[i].y = vector3.ry3 * vector3.per;
       this.tagList[i].scale = vector3.per;
       this.tagList[i].alpha = vector3.per;
       this.tagList[i].alpha = (this.tagList[i].alpha - 0.6) * (10/6);
+
     }
+    this.doPosition();
   }
 
   //TODO: doPostion()
-
-
-
-
-  positionElements () {
+  doPosition() {
+    let l = this.divObject.offsetWidth / 2;
+    let t = this.divObject.offsetHeight / 2;
+    for (let i = 0; i < this.tagList.length; i++) {
+      /*
+      'translate(' + (mcList[i].cx + l - mcList[i].offsetWidth / 2) + 'px, ' + (mcList[i].cy + t - mcList[i].offsetHeight / 2) + 'px' + ') scale(' + Math.ceil(mcList[i].scale * 100  * 0.8) / 100 + ')'
+      */
+      this.textElements[i].style.transform = 'translate(' + (this.tagList[i].cx + l - this.tagList[i].offsetWidth / 2)+ 'px, ' + (this.tagList[i].cy + t - this.tagList[i].offsetHeight/2)+ 'px ) scale(' + Math.ceil(this.tagList[i].scale * 100 * 0.7)/100 + ')';
+      console.log(this.textElements[i].style.transform);
+      this.textElements[i].style.opacity = this.tagList[i].alpha;
+      this.textElements[i].style.filter = 'alpha(opacity=' + (this.tagList[i].alpha + 0.1) * 100 + ')';
+    }
 
   }
 }
@@ -139,6 +196,34 @@ export default TagCloud;
       tg['on ' + event] = handler;
     }
   }
+  init() {
+    this.active = false;
+    let oTag = null;
+    let i = 0;
+
+    for (i = 0; i < this.aA.length; i++) {
+      oTag = {};
+      oTag.offsetWidth = this.aA[i].offsetWidth;
+      oTag.offsetHeight = this.aA[i].offsetHeight;
+      this.items.push(oTag);
+    }
+
+    this.mouseX = this.velocity * Math.sin(this.direction * (Math.PI / 180));
+    this.mouseY = -this.velocity * Math.cos(this.direction * (Math.PI / 180));
+
+    this.positionAll();
+
+    this.on(this.oDiv, 'mouseover', () => { this.active = true; });
+    this.on(this.oDiv, 'mouseout', () => { this.active = false; });
+    this.on(this.oDiv, 'mousemove', () => {
+      let oEvent = window.event || ev;
+      this.mouseX = (oEvent.clientX - (this.oDiv.offsetLeft + this.oDiv.offsetWidth/2))/5;
+      this.mouseY = (oEvent.clientY - (this.oDiv.offsetTop + this.oDiv.offsetHeight/2))/5;
+    });
+
+    this.update();
+
+  }
 
   positionAll() {
     let phi = 0;
@@ -178,34 +263,6 @@ export default TagCloud;
     }
   }
 
-  init() {
-    this.active = false;
-    let oTag = null;
-    let i = 0;
-
-    for (i = 0; i < this.aA.length; i++) {
-      oTag = {};
-      oTag.offsetWidth = this.aA[i].offsetWidth;
-      oTag.offsetHeight = this.aA[i].offsetHeight;
-      this.items.push(oTag);
-    }
-
-    this.mouseX = this.velocity * Math.sin(this.direction * (Math.PI / 180));
-    this.mouseY = -this.velocity * Math.cos(this.direction * (Math.PI / 180));
-
-    this.positionAll();
-
-    this.on(this.oDiv, 'mouseover', () => { this.active = true; });
-    this.on(this.oDiv, 'mouseout', () => { this.active = false; });
-    this.on(this.oDiv, 'mousemove', () => {
-      let oEvent = window.event || ev;
-      this.mouseX = (oEvent.clientX - (this.oDiv.offsetLeft + this.oDiv.offsetWidth/2))/5;
-      this.mouseY = (oEvent.clientY - (this.oDiv.offsetTop + this.oDiv.offsetHeight/2))/5;
-    });
-
-    this.update();
-
-  }
 
   update() {
     let a;
