e7c5db849d109dcc1b6bfd52bac9dd3e00ac742d
Michael Ehrich <michael.ehrich@krekeny.com>
2022-06-01 12:02:43 +0200

    chore(tag-cloud): grid approach wip

--- assets/js/components/tag-cloud.js
+++ assets/js/components/tag-cloud.js
@@ -5,10 +5,10 @@ class TagCloud extends BaseComponent {
 
   constructor(root, options) {
     super(root, options);
-    console.log(root)
 
     this.container = root.querySelector('.tag-cloud__container');
-    this.textElements = this.container.querySelectorAll('a');
+    this.items = this.container?.querySelectorAll('.tag-cloud__item');
+    this.textElements = this.container?.querySelectorAll('a');
     this.radius = 150;
     this.centerX = 0;
     this.centerY = 0;
@@ -19,31 +19,59 @@ class TagCloud extends BaseComponent {
     this.ellipticValue = 0.5;
 
     this.weightingElement();
-    this.fontSizeWeighting();
-    }
+    // this.fontSizeWeighting();
 
-  weightingElement () {
+    this.positionElements();
+  }
+
+  positionElements() {
+    let columnStart = 2;
+    let columnEnd = 3;
+    let rowStart = 1;
+    let rowEnd = 2;
+
+    const rowLength = 5;
+
+    for (let i = 0; i < this.items.length; i++) {
+      const current = this.tagList[i];
+      const weight = Math.ceil(current.weighting);
+      console.log('TagCloud ~ positionElements ~ weight', weight);
+
+      rowEnd = rowStart + 1;
+      columnEnd = columnStart + weight;
+
+      // TODO check if the element will fit in that row and if not go to the next row;
+      // TODO check if row is full and switch to the next one permanently
+
+      this.items[i].style = `grid-area: ${rowStart} / ${columnStart} / ${rowEnd} / ${columnEnd};`;
+
+      columnStart += weight;
+
+      if (i === 1) return;
+    }
+  }
+
+  weightingElement() {
     let avgOffsetWidth = 0;
 
     for (let j = 0; j < this.textElements.length; j++) {
-      avgOffsetWidth += this.textElements[j].offsetWidth/this.textElements.length;
+      avgOffsetWidth += this.textElements[j].offsetWidth / this.textElements.length;
     }
 
     for (let i = 0; i < this.textElements.length; i++) {
       let tag = {};
-      tag.weighting = this.textElements[i].offsetWidth/avgOffsetWidth;
+      tag.weighting = this.textElements[i].offsetWidth / avgOffsetWidth;
       this.tagList.push(tag);
     }
   }
 
-  fontSizeWeighting () {
+  fontSizeWeighting() {
     let avgFontSize = 8;
     for (let i = 0; i < this.tagList.length; i++) {
       this.textElements[i].style.fontSize = this.tagList[i].weighting * 12 + avgFontSize + 'px';
       console.log(this.textElements[i].style.fontSize);
     }
   }
-
 }
 
 export default TagCloud;
