b5dcf1914b24b39156b003a04d54b9717644f4db
Michael Ehrich <michael.ehrich@krekeny.com>
2022-06-23 12:36:50 +0200

    feat(tag-cloud): add wip simplified prototype

--- assets/js/components/tag-cloud.js
+++ assets/js/components/tag-cloud.js
@@ -14,299 +14,116 @@ class TagCloud extends BaseComponent {
     // }
 
     this.itemsPerOuterRow = 3;
-    this.itemsPerInnerRow = 5;
+    // this.itemsPerInnerRow = 5;
 
-    this.items = this.container?.querySelectorAll('.tag-cloud__item');
-    this.textElements = this.container?.querySelectorAll('a');
+    this.maxCoordinate = 40; // TODO maybe connect to the padding property to make sure it doesn't get out of bounce ?
+    this.minCoordinate = 10;
+    this.minBlur = 2;
+    this.maxBlur = 10;
+    this.maxDelay = 70;
+    this.minDelay = 550;
+    this.maxWeight = 3;
+
+    this.items = this.itemsContainer?.dataset.items ? JSON.parse(this.itemsContainer?.dataset.items) : [];
+
     this.tagList = [];
 
-    this.rowLength = 0;
     this.init();
   }
 
   init() {
-    this.weightingElement();
-    this.positionElements();
-    //this.animateGroupElements();
+    this.weightingElements();
+    this.addCorners();
+    this.appendItems();
   }
 
-  /*getGroupSize (size) {
-    return Math.ceil(this.tagList.length / size);
-  }
+  weightingElements() {
+    const weightThreshold = 8;
 
-  animateGroupElements() {
-    const maxValue = 1;
-    const minValue = 0.5;
-    let size = 3;
-    let groupSize = this.getGroupSize(size);
-    let borderReached = false;
-    let step = 0.001;
-    let backToStart = false;
+    for (let i = 0; i < this.items.length; i++) {
+      const item = this.items[i];
+      const weight = Math.ceil(item.title.length / weightThreshold);
 
-    for (let i = 0; i < this.tagList.length; i++) {
-      const currentValue = this.tagList[i].standardValue;
-      let upperBorder = currentValue > maxValue
-      let lowerBorder = currentValue < minValue
-      let aboveStart = currentValue > 0.8;
-      let belowStart = currentValue < 0.8;
-
-      if (upperBorder && !backToStart) {
-        step = -0.001;
-        backToStart = true;
-        break;
-
-      } else if (lowerBorder && !backToStart) {
-        step = 0.001;
-        backToStart = true;
-        break;
-      }
-
-      if (backToStart && aboveStart) {
-        step = -0.001;
-        break;
-
-      } else if (backToStart && belowStart) {
-        step = 0.001;
-        break;
-      }
+      this.items[i].weighting = weight > this.maxWeight ? this.maxWeight : weight;
     }
-
-    let i = 0;
-
-    while (!borderReached && i < this.tagList.length) {
-      //let bias = Math.random() * 2;
-      let bias = 1;
-      let currentValue = this.tagList[i].standardValue;
-      let newValue = 0;
-
-      newValue = (i % groupSize === 0) ? currentValue + step * bias : currentValue - step * bias;
-
-      this.tagList[i].standardValue = newValue;
-      i++;
-
-    }
-
-    this.placeGroups();
-    window.requestAnimationFrame(this.animateGroupElements.bind(this));
   }
 
-  blurCalculation (standardValue, exponent) {
-    return 1/Math.pow(standardValue, exponent);
-  }
-
-  placeGroups () {
-    for (let i = 0; i < this.tagList.length; i++) {
-      const currentItem = this.items[i];
-      const standardValue = this.tagList[i].standardValue;
-      const blurValue = this.blurCalculation(standardValue, 2.8);
-
-      currentItem.style.transform = 'scale( ' + standardValue + ')';
-      currentItem.style.filter = 'blur(' + blurValue + 'px)';
-    }
-  }*/
-
-  getList() {
-    const list = [];
-
-    let rowsToExpand = [];
-    let rows = [];
-    let rowIndex = 0;
+  getCornerPosition() {
     let weight = 0;
-    let isRowFull = false;
+    let index = 0;
 
-    for (let i = 0; i < this.tagList.length; i++) {
-      let isFirstRow = rowIndex === 0 ? true : false;
-      let isLastRow = rowIndex === this.rowLength - 1 ? true : false;
-      let itemsInRow = isFirstRow || isLastRow ? this.itemsPerOuterRow : this.itemsPerInnerRow;
+    for (let i = 0; i < this.items.length; i++) {
+      const item = this.items[i];
 
-      const currentTagWeight = this.tagList[i].weighting;
+      weight += item.weighting;
 
-      isRowFull = itemsInRow < weight + currentTagWeight;
+      if (weight > this.itemsPerOuterRow + 1) continue;
 
-      if (isRowFull) {
-        if (itemsInRow !== weight) {
-          rowsToExpand.push(rowIndex);
-        }
-
-        rows.push({ rowIndex, weight });
-
-        rowIndex++;
-      }
-
-      if (typeof list[rowIndex] !== 'object') {
-        list[rowIndex] = [];
-        weight = 0;
-      }
-
-      list[rowIndex].push(this.tagList[i]);
-      weight += currentTagWeight;
+      index = i + 1;
     }
 
-    this.rowLength = list.length;
-
-    rows.push({ rowIndex, weight });
-
-    for (let i = 0; i < rowsToExpand.length; i++) {
-      const currentIndex = rowsToExpand[i];
-      const currentRow = rows[currentIndex];
-
-      this.expandRow(currentRow, list);
-    }
-
-    let lastRow = this.rowLength - 1;
-    let beforeLastRow = lastRow - 1;
-
-    //Abfrage falls es mehr gewichte in der letzten Zeile gibt
-    let lastRowExists = typeof list[lastRow] !== 'undefined';
-    console.log(list);
-
-    if (lastRowExists && rows[lastRow].weight > this.itemsPerOuterRow) {
-      beforeLastRow = lastRow;
-      lastRow = lastRow + 1;
-      rows.push({ rowIndex: lastRow, weight: 0 });
-      this.rowLength = lastRow + 1;
-    }
-
-    console.log(list[lastRow]);
-
-    if (!lastRowExists) {
-      list[lastRow] = [];
-
-      let listObj = list[beforeLastRow];
-      let biggest = 0;
-      let biggestIndex = 0;
-
-      for (let i = 0; i < listObj.length; i++) {
-        if (listObj[i].weighting > biggest) {
-          biggest = listObj[i].weighting;
-          biggestIndex = i;
-        }
-      }
-
-      const elementToMove = listObj[biggestIndex];
-
-      rows[beforeLastRow].weight -= elementToMove.weighting;
-
-      list[lastRow].push(elementToMove);
-      list[beforeLastRow].splice(biggestIndex, 1);
-
-      this.expandRow(rows[beforeLastRow], list);
-
-      //falls die letzte Zeile fehlt und noch aufgefüllt werden muss
-      rows.push({ rowIndex: lastRow, weight: elementToMove.weighting });
-      this.expandRow(rows[lastRow], list);
-
-    } else {
-      this.expandRow(rows[beforeLastRow], list);
-      this.expandRow(rows[lastRow], list);
-    }
-    return list;
+    return index;
   }
 
-  expandRow(row, list) {
-    const index = row.rowIndex;
-    const weight = row.weight;
-    const isFirstRow = index === 0 ? true : false;
-    const isLastRow = index === this.rowLength - 1 ? true : false;
-    const itemsInRow = isFirstRow || isLastRow ? this.itemsPerOuterRow : this.itemsPerInnerRow;
+  addCorners() {
+    const corner = { weighting: 0 };
 
-    let missingWeight = itemsInRow - weight;
+    this.items.unshift(corner);
+    this.items.push(corner);
 
-    for (let i = 0; i < missingWeight; i++) {
-      console.log(index, i);
-      const element = list[index][i];
+    // TODO figure out how to get the position. maybe at the correct end or one before an item that overflows
+    // const secondCornerPosition = 3;
+    const secondCornerPosition = this.getCornerPosition();
 
-      if (!element) {
-        list[index][i - 1].weighting++;
-        continue;
+    this.items.splice(secondCornerPosition, 0, corner);
+  }
+
+  getRandomCoordinate() {
+    return this.getRandomNumberBetween(this.minCoordinate, this.maxCoordinate);
+  }
+
+  getRandomBlur() {
+    return this.getRandomNumberBetween(this.minBlur, this.maxBlur);
+  }
+
+  getRandomStart() {
+    return this.getRandomNumberBetween(this.minDelay, this.maxDelay);
+  }
+
+  getRandomNumberBetween(start, end) {
+    return Math.floor(Math.random() * (end - start + 1)) + start;
+  }
+
+  appendItems() {
+    for (let i = 0; i < this.items.length; i++) {
+      const item = this.items[i];
+      const newElement = document.createElement('li');
+
+      newElement.classList.add('tag-cloud__item');
+      newElement.setAttribute('data-weight', item.weighting);
+
+      if (item.title) {
+        const link = document.createElement('a');
+
+        link.setAttribute('href', item.link);
+
+        link.style.setProperty('--blurry-delay', `${this.getRandomStart()}ms`);
+
+        // TODO create random points more compact ?
+        link.style.setProperty('--blurry-x1', `${this.getRandomCoordinate()}px`);
+        link.style.setProperty('--blurry-y1', `${this.getRandomCoordinate()}px`);
+        link.style.setProperty('--blurry-x2', `${this.getRandomCoordinate()}px`);
+        link.style.setProperty('--blurry-y2', `${this.getRandomCoordinate()}px`);
+        link.style.setProperty('--blurry-x3', `${this.getRandomCoordinate()}px`);
+        link.style.setProperty('--blurry-y3', `${this.getRandomCoordinate()}px`);
+        link.style.setProperty('--blurry-blur', `${this.getRandomBlur()}px`);
+        link.innerText = item.title;
+        link.classList.add('tag-cloud__item-link');
+
+        newElement.appendChild(link);
       }
 
-      element.weighting++;
-    }
-
-    //update row.weight EdgeCase row gets called one time more than needed
-    row.weight = itemsInRow;
-    this.rowLength = this.getRowLength();
-  }
-
-  getRowLength() {
-    let totalWeight = 0;
-    const corners = 4;
-    const columns = 5;
-
-    for (let i = 0; i < this.tagList.length; i++) {
-      totalWeight += this.tagList[i].weighting;
-    }
-    totalWeight += corners;
-
-    return Math.ceil(totalWeight / columns);
-  }
-
-  positionElements() {
-    let columnStart = 1;
-    let columnEnd = 2;
-    let rowStart = 1;
-    let rowEnd = 2;
-
-    let currentElementsPerRow = 0;
-
-    this.rowLength = this.getRowLength();
-    let elementPerRow = 0;
-    let array = this.getList();
-    let currentElement = 0;
-
-    for (let i = 0; i < array.length; i++) {
-      let isFirstRow = i === 0 ? true : false;
-      let isLastRow = i === array.length - 1 ? true : false;
-      let eitherFirstOrLastRow = isFirstRow || isLastRow ? true : false;
-
-      columnStart = 1;
-      columnEnd = 2;
-      rowStart = 1;
-
-      elementPerRow = array[i]?.length;
-      currentElementsPerRow = 0;
-
-      while (elementPerRow > currentElementsPerRow) {
-        //Orientiert sich nicht mehr an der TagList
-        const weight = array[i][currentElementsPerRow].weighting;
-        let emptyRow = currentElementsPerRow === 0 ? true : false;
-
-        rowStart = i + 1;
-        rowEnd = rowStart + 1;
-        columnEnd = columnStart + weight;
-
-        if (eitherFirstOrLastRow) {
-          //
-          if (emptyRow) {
-            columnStart += 1;
-          }
-
-          columnEnd = columnStart + weight;
-        }
-
-        this.items[currentElement].style = `grid-area: ${rowStart} / ${columnStart} / ${rowEnd} / ${columnEnd};`;
-
-        columnStart += weight;
-        currentElement++;
-        currentElementsPerRow++;
-      }
-    }
-  }
-
-  weightingElement() {
-    let avgOffsetWidth = 0;
-
-    for (let j = 0; j < this.textElements.length; j++) {
-      avgOffsetWidth += this.textElements[j].offsetWidth / this.textElements.length;
-    }
-
-    for (let i = 0; i < this.textElements.length; i++) {
-      let tag = {};
-      tag.offsetWidth = this.textElements[i].offsetWidth;
-      tag.standardValue = 0.8;
-      tag.weighting = Math.ceil(this.textElements[i].offsetWidth / avgOffsetWidth);
-      this.tagList.push(tag);
+      this.itemsContainer.appendChild(newElement);
     }
   }
 }
--- stories/components/tagCloud.stories.js
+++ stories/components/tagCloud.stories.js
@@ -11,35 +11,72 @@ export default {
 
 const Template = (args) => createComponent(args, component);
 
-export const TagCloud = Template.bind({});
+export const ThreeRows = Template.bind({});
 
-TagCloud.args = {
-  headline: "Consultings Technology",
-  subline: "Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und",
-  items: [{title: 'Intune', link: '#'},
-          {title: 'Azure Active Directory', link: '#'},
-          {title: 'MIP', link: '#'},
-          {title: 'Security', link: '#'},
-          {title: 'MIM', link: '#'},
-          {title: 'Azure', link: '#'},
-          //{title: 'MDM', link: '#'},
-          //{title: 'Windows 365', link: '#'},
-          //{title: 'Teams', link: '#'},
-          //{title: 'M.Defender', link: '#'},
-          //{title: 'Autopilot', link: '#'},
-          //{title: 'MS 365', link: '#'},
-          //{title: 'TestCase1', link: '#'},
-          //{title: 'Azure Active Directory', link: '#'},
-          //{title: 'MIP', link: '#'},
-          //{title: 'Security', link: '#'},
-          //{title: 'MIM', link: '#'},
-          //{title: 'TestCase2', link: '#'},
-          //{title: 'MDM', link: '#'},
-          {title: 'Windows 365', link: '#'},
-          {title: 'Teams', link: '#'},
-          {title: 'TestCase3', link: '#'}
-          //{title: 'Autopilot', link: '#'},
-          //{title: 'Azure', link: '#'}
-          //{title: 'AbstractLongTestCase4', link: '#'}
-        ],
+ThreeRows.args = {
+  headline: 'Consultings Technology',
+  subline: 'Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und',
+  items: [
+    { title: 'Intune', link: '#' },
+    { title: 'Azure Active Directory', link: '#' },
+    { title: 'MIP', link: '#' },
+    { title: 'Security', link: '#' },
+    { title: 'MIM', link: '#' },
+    { title: 'Azure', link: '#' },
+    { title: 'Windows 365', link: '#' },
+    { title: 'Teams', link: '#' },
+    { title: 'TestCase3', link: '#' },
+  ],
+};
+
+export const ManyRows = Template.bind({});
+
+ManyRows.args = {
+  headline: 'Consultings Technology',
+  subline: 'Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und',
+  items: [
+    { title: 'Intune Weight', link: '#' },
+    { title: 'Azure Active Directory', link: '#' },
+    { title: 'MIP', link: '#' },
+    { title: 'Security', link: '#' },
+    { title: 'MIM', link: '#' },
+    { title: 'Azure', link: '#' },
+    { title: 'MDM', link: '#' },
+    { title: 'Windows 365', link: '#' },
+    { title: 'Teams', link: '#' },
+    { title: 'M.Defender', link: '#' },
+    { title: 'Autopilot', link: '#' },
+    { title: 'MS 365', link: '#' },
+    { title: 'TestCase1', link: '#' },
+    { title: 'Azure Active Directory', link: '#' },
+    { title: 'MIP', link: '#' },
+    { title: 'Security', link: '#' },
+    { title: 'MIM', link: '#' },
+    { title: 'TestCase2', link: '#' },
+    //{title: 'MDM', link: '#'},
+    { title: 'Windows 365', link: '#' },
+    { title: 'Teams', link: '#' },
+    { title: 'TestCase3', link: '#' },
+    //{title: 'Autopilot', link: '#'},
+    //{title: 'Azure', link: '#'}
+    //{title: 'AbstractLongTestCase4', link: '#'}
+  ],
+};
+
+export const LongWords = Template.bind({});
+
+LongWords.args = {
+  headline: 'Consultings Technology',
+  subline: 'Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und',
+  items: [
+    { title: 'Intune lorem ipsum', link: '#' },
+    { title: 'Azure Active Directory', link: '#' },
+    { title: 'Lorem ipsum dolor sit', link: '#' },
+    { title: 'Security Security Security', link: '#' },
+    { title: 'Lorem ipsum dolor', link: '#' },
+    { title: 'Azure Azure Azure', link: '#' },
+    { title: 'Windows 365 or Windows', link: '#' },
+    { title: 'Teams or Slack', link: '#' },
+    { title: 'TestCase3', link: '#' },
+  ],
 };
