c6c6b201f9b7d59f3bfc81446886e19dc2ff18e2
Michael Ehrich <michael.ehrich@krekeny.com>
2022-07-07 16:56:12 +0200

    feat(tools): adds a helper that can check if the element is in the viewport

--- assets/js/tools.js
+++ assets/js/tools.js
@@ -5,6 +5,17 @@ class Tools {
     maximumFractionDigits: 0,
   });
 
+  static isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
   static scrollIntoView(element, smooth) {
     if (element) {
       const header = document.querySelector('header');
