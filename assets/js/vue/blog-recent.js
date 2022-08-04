26cd8f38a7ef033e694168e8ea5a915081e8b999
Adnan <adnan.ahmed@glueckkanja-gab.com>
2022-08-04 14:16:08 +0200

    feat(youtube-thumbs): carousel added

--- assets/js/vue/blog-recent.js
+++ assets/js/vue/blog-recent.js
@@ -21,15 +21,15 @@ export default {
       ];
     },
 
-    hideContainerClass() {
+    hideContainerValue() {
       return `${Tools.isTrue(this.hideContainer) === true ? true : false}`;
     },
 
-    recentHeadlineClass() {
+    recentHeadlineValue() {
       return `${Tools.isTrue(this.recentHeadline) === true ? true : false}`;
     },
 
-    recentCtaClass() {
+    recentCtaValue() {
       return `${Tools.isTrue(this.recentCta) === true ? true : false}`;
     },
 
@@ -41,9 +41,64 @@ export default {
       return `${Tools.isTrue(this.slider) === true ? 'mb-6 mb-lg-0 blog-recent__slide' : 'col-sm-6 col-lg-4 mb-3 mb-sm-8'}`;
     },
 
-    postClass() {
+    postsArray() {
       return JSON.parse(this.posts);
     },
+
+    ctaParse() {
+      return JSON.parse(this.recentCta);
+    },
+
+    carouselOptions() {
+      const obj = {
+        "slidesToShow": 3,
+        "slidesToScroll": 3,
+        "prevArrow": "<span class=\"slick__arrow-left rounded-circle\"></span>",
+        "nextArrow": "<span class=\"slick__arrow-right rounded-circle\"></span>",
+        "dots": this.postsArray.length > 3 ? true : false,
+        "centerMode": false,
+        "dotsClass": "slick-pagination is-default",
+        "responsive": [{
+          "breakpoint": 1200,
+            "settings": {
+              "slidesToShow": 3,
+              "slidesToScroll": 3
+            }
+          }, {
+          "breakpoint": 992,
+          "settings": {
+            "centerMode": true,
+            "infinite": true,
+            "centerPadding": "30px",
+            "slidesToShow": 2,
+            "slidesToScroll": 2,
+            "dots": this.postsArray.length > 2 ? true : false,
+            }
+          }, {
+          "breakpoint": 768,
+          "settings": {
+            "centerMode": true,
+            "infinite": false,
+            "centerPadding": "30px",
+            "slidesToShow": 1,
+            "slidesToScroll": 1,
+            "dots": this.postsArray.length > 1 ? true : false,
+            }
+          }, {
+            "breakpoint": 576,
+            "settings": {
+              "centerMode": true,
+              "infinite": false,
+              "centerPadding": "20px",
+              "slidesToShow": 1,
+              "slidesToScroll": 1,
+              "dots": this.postsArray.length > 1 ? true : false,
+          }
+        }]
+      }
+
+      return JSON.stringify(obj);
+    }
   },
 
   methods: {
@@ -82,64 +137,19 @@ export default {
   },
 
   template: `
-    <template v-if="postClass.length > 0">
+    <template v-if="postsArray.length > 0">
       <div :class="classList">
        <div class="blog-recent__bg" :style="{ 'background-color' : bgColor  }" v-if="skinClass !== ''"></div>
-      <div class="container" v-if="hideContainerClass">
-        <div class="row" v-if="recentHeadlineClass">
+      <div class="container" v-if="hideContainerValue">
+        <div class="row" v-if="recentHeadlineValue">
           <div class="col-lg-12 col-md-10 mt-6 mt-lg-8 mb-4 mb-lg-6">
-            <headline level="h3" :text="recentHeadline">
+            <headline level="h2" :text="recentHeadline">
           </div>
         </div>
         <div :class="blogRecentContainerClass"
-          data-hs-slick-carousel-options='{
-            "slidesToShow": 3,
-            "slidesToScroll": 3,
-            "prevArrow": "<span class=\"slick__arrow-left rounded-circle\"></span>",
-            "nextArrow": "<span class=\"slick__arrow-right rounded-circle\"></span>",
-            "dots": {% if postClass.length > 3 %}true{% else %}false{% endif %},
-            "centerMode": false,
-            "dotsClass": "slick-pagination is-default",
-            "responsive": [{
-              "breakpoint": 1200,
-                "settings": {
-                  "slidesToShow": 3,
-                  "slidesToScroll": 3
-                }
-              }, {
-              "breakpoint": 992,
-              "settings": {
-                "centerMode": true,
-                "infinite": true,
-                "centerPadding": "30px",
-                "slidesToShow": 2,
-                "slidesToScroll": 2,
-                "dots": v-if="postClass.length > 2",
-                }
-              }, {
-              "breakpoint": 768,
-              "settings": {
-                "centerMode": true,
-                "infinite": false,
-                "centerPadding": "30px",
-                "slidesToShow": 1,
-                "slidesToScroll": 1,
-                "dots": v-if="postClass.length > 1",
-                }
-              }, {
-                "breakpoint": 576,
-                "settings": {
-                  "centerMode": true,
-                  "infinite": false,
-                  "centerPadding": "20px",
-                  "slidesToShow": 1,
-                  "slidesToScroll": 1,
-                  "dots": v-if="postClass.length > 1",
-              }
-            }]
-          }'
+          :data-hs-slick-carousel-options="carouselOptions"
         >
-        <template v-for="(post, index) in postClass">
+        <template v-for="(post, index) in postsArray">
           <div :class="itemClass" v-if="index <= recentLimit">
             <template v-if="post.layout == 'post'">
               <card :title="post.title" :blog-title-pic="imgUrlExist(post)" :excerpt="post.excerpt" :date="post.date" :author="post.author" :target="false" :event="false">
@@ -150,8 +160,8 @@ export default {
           </div>
         </template>
       </div>
-          <div class="blog-recent__cta-row row col-lg-12" v-if="recentCtaClass">
-            <cta :text="recentCta.text" :button="true" :skin="recentCta.skin" :width="recentCta.width" :href="recentCta.href" :external="recentCta.external" >
+          <div class="blog-recent__cta-row row col-lg-12" v-if="recentCta">
+            <cta :text="ctaParse.text" :button="true" :skin="ctaParse.skin" :width="ctaParse.width" :href="ctaParse.href" :external="ctaParse.external" >
           </div>
       </div>
 
