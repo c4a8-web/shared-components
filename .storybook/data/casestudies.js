f8d124850a98708b722aa3711c6cce1ae9d16fc1
Adnan <adnan.ahmed@glueckkanja-gab.com>
2022-08-10 13:58:38 +0200

    feat(case-studies): initial case studies

---
+++ .storybook/data/casestudies.js
@@ -0,0 +1,16 @@
+const CaseStudies = [
+  {
+    url: 'javascript:void(0);',
+    layout: 'casestudies',
+    title: 'Azure @DEKRA',
+    date: '',
+    author: '',
+    categories: '',
+    tags: '',
+    blogtitlepic: '/casestudies/casesstudy-dekra-v2.png',
+    socialimg: '/casestudies/casesstudy-dekra-v2.png',
+    excerpt: '<span class="highlight">Effiziente Umsetzung</span> der DEKRA Cloud First Journey mit DevOps und Infrastructure as Code (IaC)'
+  },
+];
+
+export default CaseStudies;
