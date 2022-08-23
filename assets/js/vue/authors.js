4cdcd4d41a6fe6157603274e8be7373e6fce69fd
Adnan <adnan.ahmed@glueckkanja-gab.com>
2022-08-23 13:50:26 +0200

    fix(blog-recent): authors fix

--- assets/js/vue/authors.js
+++ assets/js/vue/authors.js
@@ -13,6 +13,9 @@ export default {
     seperator() {
       return this.noLink ? ' & ' : ' , ';
     },
+    authorArray() {
+      return (typeof this.authorsList === 'object') ? this.authorsList : [this.authorsList];
+    }
   },
 
   methods: {
@@ -42,17 +45,17 @@ export default {
     }
   },
   template: `
-    <template v-if="authorsList">
-      <span :class="classList" v-for="author in authorsList">
+    <template v-if="authorArray">
+      <span :class="classList" v-for="author in authorArray">
         <template v-if="!noLink">
           <a :href="authorLink(author)" class="post-teaser__auto" itemprop="author" itemscope itemtype="https://schema.org/Person">
           <span itemprop="name">{{author}}</span>
           </a>
         </template>
         <template v-else>
-          <span itemprop="name">{{ authorStart(authorsList, author) }} {{author}}</span>
+          <span itemprop="name">{{ authorStart(authorArray, author) }} {{author}}</span>
         </template>
-        <template v-if="!authorsSeperator(authorsList, author)">
+        <template v-if="!authorsSeperator(authorArray, author)">
           {{ seperator }}
         </template>
       </span>
