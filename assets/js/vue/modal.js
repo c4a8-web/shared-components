f810fb6e389828baf7dbb8d67d7afee056442767
Michael Ehrich <michael.ehrich@krekeny.com>
2022-09-29 18:15:54 +0200

    feat(modal): adds slim version and layout fix

--- assets/js/vue/modal.js
+++ assets/js/vue/modal.js
@@ -1,8 +1,14 @@
+import Tools from '../tools.js';
+import State from '../state.js';
+
 export default {
   tagName: 'modal',
   computed: {
     classList() {
       return ['modal fade', 'vue-component'];
+    },
+    dialogClassList() {
+      return ['modal-dialog', `${this.slimMode ? 'modal-lg' : 'modal-xl'}`];
     },
     settings() {
       return {
@@ -14,19 +20,58 @@ export default {
     modal() {
       return this.$refs.modal;
     },
+    slimMode() {
+      return Tools.isTrue(this.slim);
+    },
+  },
+  mounted() {
+    this.bindEvents();
+  },
+  unmounted() {
+    this.observer.disconnect();
+  },
+  methods: {
+    isModalOpen() {
+      return document.body.classList.contains('modal-open') ? true : false;
+    },
+    setModalMode(mode) {
+      const html = document.querySelector('html');
+
+      if (mode) {
+        html.classList.add(State.MODAL_OPEN);
+      } else {
+        html.classList.remove(State.MODAL_OPEN);
+      }
+    },
+    bindEvents() {
+      this.observer = new MutationObserver(this.handleMutation);
+
+      this.observer.observe(document.body, { attributes: true });
+    },
+    handleMutation() {
+      this.setModalMode(this.isModalOpen());
+    },
+  },
+  data() {
+    return {
+      observer: null,
+    };
   },
   props: {
     clientName: String,
     apiUrl: String,
     jobId: String,
+    slim: {
+      default: null,
+    },
   },
   template: `
     <div :class="classList" tabindex="-1" aria-hidden="true" style="--color-icon-hover-color: var(--color-white)" ref="modal"
           v-bind="settings" >
-      <div class="modal-dialog modal-xl">
+      <div :class="dialogClassList">
         <div class="modal__content">
           <div class="modal__header">
-            <div class="modal__close">
+            <div class="modal__close" v-if="!slim">
               <icon icon="close" :hover="true" :circle="true" />
             </div>
           </div>
