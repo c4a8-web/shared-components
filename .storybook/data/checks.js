b249a9aa2200cc0e0746b8446dcbc44f576e7012
Adnan <adnan.ahmed@glueckkanja-gab.com>
2022-10-17 16:06:09 +0200

    feat(check-cards): Initial Commit

---
+++ .storybook/data/checks.js
@@ -0,0 +1,16 @@
+const Check = [
+  {
+    url: "/",
+    title: "glueckkanja-gab Quick-Check",
+    subline: "Bei glueckkanja-gab sitzt du nicht nur direkt an der Microsoft-Quelle- du profitierst auch von einer Microsoft-Expertise",
+    picture: "https://res.cloudinary.com/glueckkanja/image/upload/v1620393502/blog/heads/head-emergency-calls.jpg",
+    subpoints: ["Bei glueckkanja-gab sitzt du nicht nur direkt an der Mircrosoft-Quelle- du profitierst auch von einer Microsoft-Expertise, die so kaum ein anderes IT-Beratungsunternehmen in Deutschland hat", "Dir stehen bei glueckkanja-gab viele Türen offen und", "Auch wenn du heute nicht so weit bist"]
+  },
+  {
+    url: "/",
+    title: "glueckkanja-gab Quick-Check",
+    subline: "Bei glueckkanja-gab sitzt du nicht nur direkt an der Microsoft-Quelle- du profitierst auch von einer Microsoft-Expertise",
+    picture: "https://res.cloudinary.com/glueckkanja/image/upload/v1620393502/blog/heads/head-emergency-calls.jpg",
+    subpoints: ["Bei glueckkanja-gab sitzt du nicht nur direkt an der Mircrosoft-Quelle- du profitierst auch von einer Microsoft-Expertise, die so kaum ein anderes IT-Beratungsunternehmen in Deutschland hat", "Dir stehen bei glueckkanja-gab viele Türen offen und", "Auch wenn du heute nicht so weit bist"]
+  }
+]
---
+++ assets/js/vue/check-card.js
@@ -0,0 +1,120 @@
+import Tools from '../tools.js'
+
+export default {
+  tagName: 'check-card',
+  computed: {
+    classList() {
+      return [
+        'check-card',
+        'vue-component',
+      ];
+    },
+    checkCardsContainerClass() {
+      return [
+        'check-card__container',
+        `${Tools.isTrue(this.slider) === true ? 'js-slick-carousel' : 'row mb-3'}`,
+        'vue-component'
+      ]
+    },
+    headlineLevelValue() {
+      return this.headlineLevel ? this.headlineLevel : 'h3'
+    },
+    headlineClassesValue() {
+      return `h2-font-size ${this.headlineClasses ? this.headlineClasses : ''}`
+    },
+    sublineClassesValue() {
+      return `check-card__subline ${this.sublineClasses ? this.sublineClasses : 'font-size-2'}`
+    },
+    itemClass() {
+      return `${
+        Tools.isTrue(this.slider) === true ? 'mb-6 mb-lg-0 blog-recent__slide' : 'col-sm-6 col-lg-4 mb-3 mb-sm-8'
+      }`;
+    },
+  },
+  carouselOptions() {
+    const obj = {
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      prevArrow: '<span class="slick__arrow-left rounded-circle"></span>',
+      nextArrow: '<span class="slick__arrow-right rounded-circle"></span>',
+      dots: this.postsArray.length > 3 ? true : false,
+      centerMode: false,
+      dotsClass: 'slick-pagination is-default',
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+          },
+        },
+        {
+          breakpoint: 992,
+          settings: {
+            centerMode: true,
+            infinite: true,
+            centerPadding: '30px',
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            dots: this.postsArray.length > 2 ? true : false,
+          },
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            centerMode: true,
+            infinite: false,
+            centerPadding: '30px',
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: this.postsArray.length > 1 ? true : false,
+          },
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            centerMode: true,
+            infinite: false,
+            centerPadding: '20px',
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: this.postsArray.length > 1 ? true : false,
+          },
+        },
+      ],
+    };
+    return JSON.stringify(obj);
+  },
+  methods: {
+  },
+  props: {
+    slider: {
+      default: null,
+    },
+    headline: String,
+    headlineLevel: String,
+    headlineClasses: String,
+    subline: String,
+    sublineClasses: String,
+    check: {
+      default: null,
+    }
+  },
+  template: `
+  <div :class="classList">
+    <div class="row" v-if="headline">
+      <div class="col-lg-12 col-md-10 mt-6 mt-lg-8 mb-4 mb-lg-6">
+        <headline :level="headlineLevelValue" :text="headline" :classes="headlineClassesValue" />
+        <span v-if="subline" :class="sublineClassesValue"> {{ subline }}</span>
+      </div>
+    </div>
+    <div :class="checkCardsContainerClass" :data-hs-slick-carousel-options="carouselOptions">
+      <template v-for="(check, index) in checkArray">
+        <div :class="itemClass" v-if="index <= limit">
+          <card :url="check.url" :title="check.title" :blog-title-pic="check.picture" :excerpt="check.subline" :subpoints="check.subpoints" />
+        </div>
+      </template>
+    </div>
+  </div>
+  `,
+};
