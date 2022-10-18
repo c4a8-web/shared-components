ee50ffb34065434eb5403042ddb55d9e6f965d02
Davor Culibrk <davor.culibrk@krekeny.com>
2022-10-18 18:10:37 +0200

    feat(L-block): add dynamic colors

--- assets/js/vue/shapes/l-block.js
+++ assets/js/vue/shapes/l-block.js
@@ -34,29 +34,27 @@ export default {
 
       for (const prop of Object.keys(shape)) {
         props[prop] = shape[prop];
-        console.log('prop', prop);
-        console.log('value', shape[prop]);
       }
 
       return props;
     },
 
-    getShapeDataNew(id) {
+    getShapeData(id) {
       const shape = (this.shapes && this.shapes[id]) ?? {};
 
       return this.allProps(shape);
     },
-    getShapeData(id) {
-      const shape = (this.shapes && this.shapes[id]) ?? {};
+    // getShapeData(id) {
+    //   const shape = (this.shapes && this.shapes[id]) ?? {};
 
-      return {
-        name: shape.name,
-        backgroundColor: shape.backgroundColor ? shape.backgroundColor : null,
-        foregroundColor: shape.foregroundColor ? shape.foregroundColor : null,
-        thirdColor: shape.thirdColor ? shape.thirdColor : null,
-        component: shape.component ? shape.component : this.defaultComponent(id),
-      };
-    },
+    //   return {
+    //     name: shape.name,
+    //     backgroundColor: shape.backgroundColor ? shape.backgroundColor : null,
+    //     foregroundColor: shape.foregroundColor ? shape.foregroundColor : null,
+    //     thirdColor: shape.thirdColor ? shape.thirdColor : null,
+    //     component: shape.component ? shape.component : this.defaultComponent(id),
+    //   };
+    // },
     defaultComponent(id) {
       // TODO component :is="component"
 
@@ -82,33 +80,33 @@ export default {
       <component
         transform="translate(0 400)"
         :is="firstShape?.name"
-        v-bind="getShapeDataNew(0)">
+        v-bind="firstShape">
       </component>
 
       <component
         transform="translate(400 800)"
         :is="secondShape?.name"
-        v-bind="getShapeDataNew(1)">
+        v-bind="secondShape">
       </component>
 
       <component
         transform="translate(0 0)"
         :is="thirdShape?.name"
-        v-bind="getShapeDataNew(2)">
+        v-bind="thirdShape">
       </component>
 
       <component
         v-if="fourthShape.name"
         transform="translate(-400 400)"
         :is="fourthShape?.name"
-        v-bind="getShapeDataNew(2)">
+        v-bind="fourthShape">
       </component>
       <g v-else transform="translate (-400 400)" >
-        <path fill="#fcd116" d="M 400 800 h 400 v -400 H 400 z"/>
-        <path fill="#f8842c" d="M 600 800 H 400 L 600 600 l 0 200 z"/>
-        <path fill="#ffffff" d="m 800 800 l -200 -200 H 600 V 800 z"/>
-        <path fill="#f8842c" d="m 400 600 l 200 -200 H 600 V 600 z"/>
-        <path fill="#ffffff" d="m 800 600 l -200 -200 H 600 V 600 z"/>
+        <path :fill="fourthShape?.backgroundColor" d="M 400 800 h 400 v -400 H 400 z"/>
+        <path :fill="fourthShape?.leftTriangleColor" d="M 600 800 H 400 L 600 600 l 0 200 z"/>
+        <path :fill="fourthShape?.rightTriangleColor" d="m 800 800 l -200 -200 H 600 V 800 z"/>
+        <path :fill="fourthShape?.leftTriangleColor" d="m 400 600 l 200 -200 H 600 V 600 z"/>
+        <path :fill="fourthShape?.rightTriangleColor" d="m 800 600 l -200 -200 H 600 V 600 z"/>
       </g>
 
     </svg>
