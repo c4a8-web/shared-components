3771926db6d372de04aeb5ae9b398df6f7744db7
Michael Ehrich <michael.ehrich@krekeny.com>
2022-10-24 15:25:44 +0200

    fix(check-card): passes data properties now

--- assets/js/vue/check-card.js
+++ assets/js/vue/check-card.js
@@ -1,96 +1,85 @@
-import Tools from '../tools.js'
+import Tools from '../tools.js';
 
+// TODO rename component
 export default {
   tagName: 'check-card',
   computed: {
     classList() {
-      return [
-        'check-card',
-        'vue-component',
-      ];
+      return ['check-card vue-component'];
     },
     checkCardsContainerClass() {
-      return [
-        'check-card__container',
-        `${Tools.isTrue(this.slider) === true ? 'js-slick-carousel justify-content-lg-center' : 'row mb-3'}`,
-        'vue-component'
-      ]
+      return ['check-card__container js-slick-carousel'];
     },
     headlineLevelValue() {
-      return this.headlineLevel ? this.headlineLevel : 'h3'
+      return this.headlineLevel ? this.headlineLevel : 'h3';
     },
     headlineClassesValue() {
-      return `h2-font-size ${this.headlineClasses ? this.headlineClasses : ''}`
+      return `h2-font-size ${this.headlineClasses ? this.headlineClasses : ''}`;
     },
     sublineClassesValue() {
-      return `check-card__subline ${this.sublineClasses ? this.sublineClasses : 'font-size-2'}`
+      return `check-card__subline ${this.sublineClasses ? this.sublineClasses : 'font-size-2'}`;
     },
     itemClass() {
-      return `${
-        Tools.isTrue(this.slider) === true ? ' check-card__slide' : 'col-sm-6 col-lg-4'
-      }`;
+      return 'check-card__slide';
+    },
+    carouselOptions() {
+      const obj = {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        prevArrow: '<span class="slick__arrow-left rounded-circle"></span>',
+        nextArrow: '<span class="slick__arrow-right rounded-circle"></span>',
+        dots: this.checks.length > 2 ? true : false,
+        centerMode: false,
+        dotsClass: 'slick-pagination is-default',
+        responsive: [
+          {
+            breakpoint: 1200,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            },
+          },
+          {
+            breakpoint: 992,
+            settings: {
+              centerMode: true,
+              infinite: true,
+              centerPadding: '30px',
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              dots: this.checks.length > 2 ? true : false,
+            },
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              centerMode: true,
+              infinite: false,
+              centerPadding: '30px',
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              dots: this.checks.length > 1 ? true : false,
+            },
+          },
+          {
+            breakpoint: 576,
+            settings: {
+              centerMode: true,
+              infinite: false,
+              centerPadding: '20px',
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              dots: this.checks.length > 1 ? true : false,
+            },
+          },
+        ],
+      };
+
+      return JSON.stringify(obj);
     },
   },
-  carouselOptions() {
-    const obj = {
-      slidesToShow: 2,
-      slidesToScroll: 2,
-      prevArrow: '<span class="slick__arrow-left rounded-circle"></span>',
-      nextArrow: '<span class="slick__arrow-right rounded-circle"></span>',
-      dots: this.checks.length > 2 ? true : false,
-      centerMode: false,
-      dotsClass: 'slick-pagination is-default',
-      responsive: [
-        {
-          breakpoint: 1200,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
-          },
-        },
-        {
-          breakpoint: 992,
-          settings: {
-            centerMode: true,
-            infinite: true,
-            centerPadding: '30px',
-            slidesToShow: 2,
-            slidesToScroll: 2,
-            dots: this.checks.length > 2 ? true : false,
-          },
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            centerMode: true,
-            infinite: false,
-            centerPadding: '30px',
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            dots: this.checks.length > 1 ? true : false,
-          },
-        },
-        {
-          breakpoint: 576,
-          settings: {
-            centerMode: true,
-            infinite: false,
-            centerPadding: '20px',
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            dots: this.checks.length > 1 ? true : false,
-          },
-        },
-      ],
-    };
-    return JSON.stringify(obj);
-  },
-  methods: {
-  },
+  methods: {},
   props: {
-    slider: {
-      default: null,
-    },
     headline: String,
     headlineLevel: String,
     headlineClasses: String,
@@ -98,25 +87,25 @@ export default {
     sublineClasses: String,
     checks: {
       default: null,
-    }
+    },
   },
   template: `
-  <div :class="classList">
-    <div class="container">
-      <div class="row" v-if="headline">
-        <div class="col-lg-12 col-md-10 mt-6 mt-lg-8 mb-4 mb-lg-6">
-          <headline :level="headlineLevelValue" :text="headline" :classes="headlineClassesValue" />
-          <span v-if="subline" :class="sublineClassesValue"> {{ subline }}</span>
+    <div :class="classList">
+      <div class="container">
+        <div class="row" v-if="headline">
+          <div class="col-lg-12 col-md-10 mt-6 mt-lg-8 mb-4 mb-lg-6">
+            <headline :level="headlineLevelValue" :text="headline" :classes="headlineClassesValue" />
+            <span v-if="subline" :class="sublineClassesValue"> {{ subline }}</span>
+          </div>
+        </div>
+        <div :class="checkCardsContainerClass" :data-hs-slick-carousel-options="carouselOptions">
+          <template v-for="(check, index) in checks">
+            <div :class="itemClass" v-if="index <= 1">
+              <card :url="check.url" :title="check.title" :blog-title-pic="check.picture" :excerpt="check.subline" :sub-points="check.subpoints" long=true />
+            </div>
+          </template>
         </div>
       </div>
-      <div :class="checkCardsContainerClass" :data-hs-slick-carousel-options="carouselOptions">
-        <template v-for="(check, index) in checks">
-          <div :class="itemClass" v-if="index <= 1">
-            <card :url="check.url" :title="check.title" :blog-title-pic="check.picture" :excerpt="check.subline" :sub-points="check.subpoints" :long="check.long" />
-          </div>
-        </template>
-      </div>
     </div>
-  </div>
   `,
 };
