8ce08e07f266c0cc7e20652e13154f643ee54382
Michael Ehrich <michael.ehrich@krekeny.com>
2022-11-01 16:10:05 +0100

    feat(check-card): adds 3 slides to desktop

--- .storybook/data/checks.js
+++ .storybook/data/checks.js
@@ -12,8 +12,19 @@ const Checks = [
     ],
   },
   {
-    url: '/',
     title: 'glueckkanja-gab Detailed-Check',
+    subline:
+      'Bei glueckkanja-gab sitzt du nicht nur direkt an der Microsoft-Quelle- du profitierst auch von einer Microsoft-Expertise',
+    picture: '/blog/pics/check-quick.png',
+    subpoints: [
+      'Bei glueckkanja-gab sitzt du nicht nur direkt an der Mircrosoft-Quelle- du profitierst auch von einer Microsoft-Expertise, die so kaum ein anderes IT-Beratungsunternehmen in Deutschland hat',
+      'Dir stehen bei glueckkanja-gab viele Türen offen und',
+      'Auch wenn du heute nicht so weit bist - wir',
+      'machen dich zum Experten für unsere Produkte - wenn du die richtigen Fähigkeiten mitbringst',
+    ],
+  },
+  {
+    title: 'glueckkanja-gab 3',
     subline:
       'Bei glueckkanja-gab sitzt du nicht nur direkt an der Microsoft-Quelle- du profitierst auch von einer Microsoft-Expertise',
     picture: '/blog/pics/check-quick.png',
