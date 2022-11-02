7a808563075bb837a941d27c32fa12c467fbecf3
Michael Ehrich <michael.ehrich@krekeny.com>
2022-11-02 10:31:02 +0100

    feat(link-list): adds no title variant

--- stories/components/linkList.stories.js
+++ stories/components/linkList.stories.js
@@ -80,3 +80,64 @@ LinkList.args = {
     ],
   },
 };
+
+export const NoTitle = Template.bind({});
+
+NoTitle.args = {
+  list: {
+    children: [
+      {
+        name: 'clear-spend-go-card',
+        languages: {
+          de: {
+            title: 'clear lorem ipsum',
+            url: 'javascript:void(0);',
+          },
+          en: {
+            title: 'clear dolor sit',
+            url: 'javascript:void(0);',
+          },
+        },
+      },
+      {
+        name: 'consulting-services',
+        languages: {
+          de: {
+            title: 'Consulting Services DE',
+            url: '/de/modern-workplace/consulting-services/',
+          },
+          en: {
+            title: 'Consulting Services EN',
+            url: '/en/modern-workplace/consulting-services/',
+          },
+        },
+      },
+      {
+        name: 'lorem2',
+        languages: {
+          de: {
+            title: 'lorem ipsum 2',
+            url: 'javascript:void(0);',
+          },
+          en: {
+            title: 'dolor sit 2',
+            url: 'javascript:void(0);',
+          },
+        },
+      },
+      {
+        name: 'lorem3',
+        languages: {
+          de: {
+            title: 'lorem ipsum 3',
+            url: 'javascript:void(0);',
+          },
+          en: {
+            title: 'dolor sit 3',
+            url: 'javascript:void(0);',
+          },
+        },
+      },
+    ],
+  },
+};
