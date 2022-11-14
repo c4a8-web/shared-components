4c198b6edc97065e3ae76bacebf911c071822cb4
Adnan <adnan.ahmed@glueckkanja-gab.com>
2022-11-14 15:39:53 +0100

    feat(testimonial-list): Aspect-ratio

--- assets/js/vue/testimonial-list.js
+++ assets/js/vue/testimonial-list.js
@@ -23,34 +23,3 @@ export default {
   </div>
   `,
 };
-/*
-<div class="testimonial-list container">
-  <div class="testimonial-list__contents row">
-    {% assign idx = 0 %}
-    {% for testimonial in  testimonialListContents %}
-
-      {% if testimonialListEven == true %}
-        {% assign testimonialListContentClass = " testimonial-list__content--right " %}
-        {% assign testimonialListEven = false %}
-      {% else %}
-        {% assign testimonialListContentClass = "  testimonial-list__content--left " %}
-        {% assign testimonialListEven = true %}
-      {% endif %}
-      <div class="testimonial-list__content row {{ testimonialListContentClass }}">
-        {% unless testimonial.hidden %}
-        {%
-          include testimonial-teaser.html
-          href=testimonial.url
-          name=testimonial.name
-          title=testimonial.title
-          img=testimonial.contentImg
-          cornerImg=testimonial.cornerImg
-          bgColor=testimonial.bgColor
-          bgColorHover=testimonial.bgColorHover
-        %}
-        {% endunless %}
-      </div>
-    {% endfor %}
-  </div>
-</div>
-*/
