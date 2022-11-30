35b1a5adfb71a1ae6160dfcb4ae8b419f38224ad
Michael Ehrich <michael.ehrich@krekeny.com>
2022-11-30 18:18:34 +0100

    refactor(testimonials-detail): changes object structure + cleanup

--- stories/components/testimonialsDetail.stories.js
+++ stories/components/testimonialsDetail.stories.js
@@ -11,17 +11,18 @@ export default {
 
 const Template = (args) => createComponent(args, component);
 
-const baseArgs = {};
+const baseArgs = {
+  name: 'Matti Puolitaival',
+  title: 'Senior Consultant Azure',
+  location: 'Finnland',
+};
 
 export const TestimonialsDetail = Template.bind({});
 
 TestimonialsDetail.args = {
-  name: 'Matti Puolitaival',
-  title: 'Senior Consultant Azure',
-  location: 'Finnland',
+  ...baseArgs,
   description:
     '<p>Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und Konsonantien leben die Blindtexte. Abgeschieden wohnen sie in Buchstabhausen an der Küste des Semantik, eines großen Sprachozeans.</p> ',
-  ...baseArgs,
   image: {
     img: 'people/testimonials/matti-puolitaival.png',
     alt: 'lorem ipsum',
@@ -31,19 +32,15 @@ TestimonialsDetail.args = {
 export const TestimonialsDetailColor = Template.bind({});
 
 TestimonialsDetailColor.args = {
-  name: 'Matti Puolitaival',
-  title: 'Senior Consultant Azure',
-  location: 'Finnland',
+  ...baseArgs,
   description:
     '<p>Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und Konsonantien leben die Blindtexte. Abgeschieden wohnen sie in Buchstabhausen an der Küste des Semantik, eines großen Sprachozeans.</p> ',
-  ...baseArgs,
+  detailColor: '--color-green',
   image: {
-    detailColor: 'green',
     img: 'people/testimonials/matti-puolitaival.png',
     alt: 'lorem ipsum',
   },
 };
-
 
 export const TestimonialsDetailSafety = Template.bind({});
 
@@ -54,56 +51,6 @@ TestimonialsDetailSafety.args = {
     img: 'people/testimonials/matti-puolitaival.png',
     alt: 'lorem ipsum',
   },
-  safety: {
-    headline: "Grünten - Wächter des Allgäus",
-    headlineLevel: "h3",
-    subline: "Grünten | Kranzegg | Allgäu",
-    copy: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata,",
-    facts: [
-      {
-        contentTitle: "Lage",
-        contentTitleLevel: "h4",
-        content: ["Anfahrt zum Parkplatz Kammeregg (87549 Rettenberg)", "Startpunkt Alpe Kammeregg (1.079m)", "Einkehr Grüntenhütte (1.477m)", "Gipfel (Kriegerdenkmal, 1.738m)"],
-      },
-      {
-        contentTitle: "Streckenlänge",
-        contentTitleLevel: "h4",
-        content: "Ca. 7,0 Kilometer",
-      },
-      {
-        contentTitle: "Höhenmeter",
-        contentTitleLevel: "h4",
-        content: "659 hm",
-      },
-      {
-        contentTitle: "Schwierigkeitsgrad",
-        contentTitleLevel: "h4",
-        content: "mittel",
-      },
-      {
-        contentTitle: "Unbedingt mitnehmen",
-        contentTitleLevel: "h4",
-        content: ["festes Schuhwerk", "Wanderstöcke", "Winddichte Kleidung", '"Hierabiera" (Stirnlampe)'],
-        img: ['/people/testimonials/matti-puolitaival.png', '/people/testimonials/matti-puolitaival.png', '/people/testimonials/matti-puolitaival.png'],
-        cloudinary: true,
-      },
-      {
-        contentTitle: "Tipp",
-        contentTitleLevel: "h4",
-        content: ["Zum Sonnenaufgang am Gipfel sein"]
-      }
-    ],
-    googleMaps: {
-      headline: {
-        text: 'Siehe hier die Location',
-        level: 'h3',
-      },
-      address: {
-        name: 'glueckkanja-gab AG',
-        street: 'Kaiserstraße 39',
-        postalCode: '63065',
-        city: 'Offenbach am Main',
-      },
-    },
-  }
-}
+  description:
+    '<p>Wer auf der Autobahn in Richtung Allgäu fährt, entdeckt ihn sofort: den Grünten, Wächter des Allgäus und Kranzeggs markanter Hausberg mit dem knapp 100 m hohen Sendemast des Bayerischen Rundfunks. Zwei unterschiedliche Wanderrouten stehen Naturliebhabern, die sensationelle optische Eindrücke und die Stille der Natur suchen, hier zur Auswahl. </p> <h2>Wandern mit Allgäuer Dialekt.</h2> <p>Los geht’s am besten ganz klassisch noch vor dem Frühstück mit umgeschnallter Hierabiera (Allgäuer Dialekt für Stirnlampe) von der Alpe Kammeregg aus. Die erste Route führt über einen geteerten Weg bis hoch zur Grüntenhütte. Auf Route Nummer 2 wandert man querfeldein mitten durch Wald, über lauschige Wiesen und vorbei an romantischen Bächlein, bis man etwas oberhalb der Grüntenhütte herauskommt. Ein idealer Spot für einen kleinen Vesper (Allgäuer Dialekt für Brotzeit) mit tollem Blick ins Tal. Frisch gestärkt kann man von hier aus dann über einen Schotter-Pfad die Gipfelstation erreichen. Vor allem auf dem letzten Abschnitt ist aber Trittsicherheit gefragt: kurz vor dem Gipfel werden die Wege nämlich sehr schmal. Wer mag, kann hier auch ein wenig kraxeln (Allgäuer Dialekt für… na, das wissen Sie aber, oder!). Auf den Gipfel rauf geht’s über eine Steintreppe. Hier oben gibt’s als Belohnung dann nicht nur das imposante Jägerdenkmal, sondern auch einen einmaligen 360-Grad-Blick auf das gesamte Allgäu, glutroten Sonnenaufgang inklusive. </p> <h2>GUT ZU WISSEN!</h2> <h4>Lage: </h4> <ul class="richtext"> <li>Anfahrt zum Parkplatz Kammeregg, 87549 Rettenberg</li> <li>Startpunkt Alpe Kammeregg (1.079 m)</li> <li>Einkehr Grüntenhütte (1.477 m) </li> <li>Gipfel (Kriegerdenkmal, 1.738 m) </li> </ul> <h4>Streckenlänge: </h4> <ul class="richtext"> <li>Ca. 7,0 Kilometer</li> </ul> <h4>Höhenmeter: </h4> <ul class="richtext"> <li>659 hm</li> </ul> <h4>Schwierigkeitsgrad: </h4> <ul class="richtext"> <li>mittel</li> </ul> <h4>Unbedingt mitnehmen: </h4> <ul class="richtext"> <li>festes Schuhwerk </li> <li>Wanderstöcke </li> <li>Winddichte Kleidung </li> <li>Hierabiera (Stirnlampe) </li> </ul> <h4>Tipp: </h4> <ul class="richtext"> <li>Zum Sonnenaufgang am Gipfel sein</li> </ul> <div class="row mb-5"> <div class="col-md p-1"> </div> <div class="col-md p-1"> </div> </div> <h4>Linkliste</h4> <a href="https://www.alpsee-gruenten.de/urlaub-im-allgaeu/gruenten.html">Der Grünten: Wächter des Allgäus (alpsee-gruenten.de) </a>',
+};
