afa5e3133048f602f9c0ad2b7f33fc557e38735f
Michael Ehrich <michael.ehrich@krekeny.com>
2022-12-12 16:21:29 +0100

    feat(youtube-player): adds yt player tools

---
+++ assets/js/youtube-player.js
@@ -0,0 +1,10 @@
+class YoutubePlayer {
+  static getEmbedSrc(videoId, autoplay) {
+    const origin = encodeURIComponent(document.location.origin);
+    const referrer = encodeURIComponent(document.location.href);
+
+    return `//www.youtube-nocookie.com/embed/${videoId}?origin=${origin}&autoplay=${autoplay}&widget_referrer=${referrer}&enablejsapi=1&widgetid=1`;
+  }
+}
+
+export default YoutubePlayer;
