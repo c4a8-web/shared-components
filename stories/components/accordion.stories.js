2937b51067d6d545512448fda57dc5208ecb9669
Adnan <adnan.ahmed@glueckkanja-gab.com>
2022-12-19 14:30:35 +0100

    feat(accordion): Added accordion to shared-components

---
+++ stories/components/accordion.stories.js
@@ -0,0 +1,50 @@
+import { createComponent, getTitle } from '../../.storybook/templates';
+import { includesaccordionhtml as component } from '../../.storybook/generatedIncludes';
+
+const options = getTitle({
+  title: 'Accordion',
+});
+
+export default {
+  ...options,
+};
+
+const Template = (args) => createComponent(args, component);
+
+export const Accordion = Template.bind({});
+Accordion.args = {
+  accordion: {
+    headline: 'Features of the Admin Portal',
+    id: 'featureAccordion',
+    tabs: [
+      {
+        headline: 'WIFI and LAN policy creation',
+        content: 'Easy download of XML files to generate WIFI and LAN policies',
+        expanded: true,
+        image: '/products/radius/radius-wifi-lan-policy.gif',
+        alt: 'WIFI and LAN policy creation',
+      },
+      {
+        headline: 'WIFI and LAN policy creation',
+        content: 'Easy download of XML files to generate WIFI and LAN policies',
+        expanded: true,
+        image: '/products/radius/radius-wifi-lan-policy.gif',
+        alt: 'WIFI and LAN policy creation',
+      },
+      {
+        headline: 'WIFI and LAN policy creation',
+        content: 'Easy download of XML files to generate WIFI and LAN policies',
+        expanded: true,
+        image: '/products/radius/radius-wifi-lan-policy.gif',
+        alt: 'WIFI and LAN policy creation',
+      },
+      {
+        headline: 'WIFI and LAN policy creation',
+        content: 'Easy download of XML files to generate WIFI and LAN policies',
+        expanded: true,
+        image: '/products/radius/radius-wifi-lan-policy.gif',
+        alt: 'WIFI and LAN policy creation',
+      },
+    ],
+  },
+};
