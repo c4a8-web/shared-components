7a87978d3c7c49ea467a47a2c628155f01264aeb
Michael Ehrich <michael.ehrich@krekeny.com>
2022-12-22 13:47:35 +0100

    fix(accordion): changes icon size

--- assets/js/vue/icons/minus.js
+++ assets/js/vue/icons/minus.js
@@ -13,9 +13,9 @@ export default {
     color: String,
   },
   template: `
-  <svg v-bind="settings" :style="style" viewBox="0 0 24 2" fill="none">
-    <path fill-rule="evenodd" clip-rule="evenodd" d="M23 1H1Z" fill="#0072C6"/>
-    <path d="M23 1H1" stroke-linecap="round"/>
-  </svg>
+    <svg v-bind="settings" :style="style" viewBox="0 0 24 2" fill="none">
+      <path fill-rule="evenodd" clip-rule="evenodd" d="M23 1H1Z" fill="#0072C6"/>
+      <path d="M23 1H1" stroke-linecap="round"/>
+    </svg>
  `,
 };
--- assets/js/vue/icons/plus.js
+++ assets/js/vue/icons/plus.js
@@ -13,9 +13,9 @@ export default {
     color: String,
   },
   template: `
-  <svg v-bind="settings" :style="style" viewBox="0 0 24 24" fill="none">
-    <path d="M12 1V23" stroke="#000520" stroke-linecap="round"/>
-    <path d="M23 12H1" stroke="#000520" stroke-linecap="round"/>
-  </svg>
+    <svg v-bind="settings" :style="style" viewBox="0 0 24 24" fill="none">
+      <path d="M12 1V23" stroke="#000520" stroke-linecap="round"/>
+      <path d="M23 12H1" stroke="#000520" stroke-linecap="round"/>
+    </svg>
  `,
 };
