c9b6fbbbea162f319000761fc281c8967e697ce8
Michael Ehrich <michael.ehrich@krekeny.com>
2023-01-10 16:56:28 +0100

    feat(img): adds wip srcset generation with preset

--- assets/js/vue/img.js
+++ assets/js/vue/img.js
@@ -1,6 +1,7 @@
 // This is a very basic img component and doesn't have the logic that the ruby version has.
 // we need to build that logic to implement a somewhat same version of that component
 import Tools from '../tools.js';
+import Presets from '../presets.js';
 
 const basePath = 'https://res.cloudinary.com/c4a8/image/upload/';
 
@@ -8,7 +9,11 @@ export default {
   tagName: 'v-img',
   computed: {
     classList() {
-      return ['', 'vue-component'];
+      return [
+        'v-img',
+        'vue-component',
+        this.cloudinary && !this.isGif() ? `no-small img-responsive ${this.class ? this.class : ''}` : '',
+      ];
     },
     source() {
       if (Tools.isTrue(this.cloudinary)) {
@@ -20,13 +25,47 @@ export default {
     loading() {
       return this.lazy ? 'lazy' : null;
     },
+    srcset() {
+      if (!this.cloudinary) return;
+
+      return;
+
+      return 'srcset';
+    },
+    sizes() {
+      if (!this.cloudinary) return;
+
+      const Preset = this.getPreset();
+
+      return Preset.sizes;
+    },
+    width() {
+      // TODO implement width calculation
+    },
+    height() {
+      // TODO implement height calculation
+    },
+  },
+  methods: {
+    isGif() {
+      const extension = this.img.split('.')[1];
+
+      return extension.toLowerCase() === 'gif';
+    },
+    getPreset() {
+      const selectedPreset = Presets[this.preset];
+
+      return selectedPreset ? selectedPreset : Presets['default'];
+    },
   },
   props: {
     img: String,
     cloudinary: Boolean,
     lazy: Boolean,
+    class: String,
+    preset: String,
   },
   template: `
-    <img :src="source" :loading="loading">
+    <img :src="source" :loading="loading" :class="classList" :srcset="srcset" :sizes="sizes">
   `,
 };
