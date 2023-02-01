22a5f68c179738f182e829c28307cd9a2d099f53
Michael Ehrich <michael.ehrich@krekeny.com>
2023-02-01 11:18:48 +0100

    feat(plus-minus): adds animation to icon

--- assets/js/vue/icons/minus.js
+++ assets/js/vue/icons/minus.js
@@ -13,9 +13,8 @@ export default {
     color: String,
   },
   template: `
-    <svg v-bind="settings" :style="style" viewBox="0 0 24 2" fill="none">
-      <path fill-rule="evenodd" clip-rule="evenodd" d="M23 1H1Z" fill="#0072C6"/>
-      <path d="M23 1H1" stroke-linecap="round"/>
+    <svg v-bind="settings" :style="style" viewBox="0 0 24 24" fill="none">
+      <path d="M23 12H1" stroke="#000520" stroke-linecap="round"/>
     </svg>
  `,
 };
