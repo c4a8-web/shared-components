5f05c722700948a8ecc9ed3547a291bb392885cf
Michael Ehrich <michael.ehrich@krekeny.com>
2023-02-15 17:05:22 +0100

    feat(absolute-scroller): wip + multiple elements works  kindaish

--- assets/js/absolute-scroller.js
+++ assets/js/absolute-scroller.js
@@ -13,6 +13,7 @@ class AbsoluteScroller {
     this.firstChild = document.querySelector('main > *:first-child');
 
     this.isUpdating = false;
+    this.maxPercentage = 100;
 
     this.setup();
     this.bindEvents();
@@ -20,6 +21,8 @@ class AbsoluteScroller {
   }
 
   // TODO handle resize for spacer
+  // TODO test with new header
+  // TODO check mobile
 
   bindEvents() {
     window.addEventListener('scroll', this.handleScroll.bind(this));
@@ -33,13 +36,21 @@ class AbsoluteScroller {
     window.requestAnimationFrame(this.setStickyPosition.bind(this));
   }
 
-  updateClipPath(position, topValue) {
-    const offsetTop = this.spacer.offsetTop - topValue;
-    const offsetBottom = offsetTop + this.spacer.offsetHeight;
+  getHeaderHeight() {
+    return this.isHeaderVisible() ? document.querySelector('header')?.offsetHeight || 0 : 0;
+  }
 
-    const step = (offsetBottom - offsetTop) / 100;
-    const percentage = (position - offsetTop) / step;
-    const clipPath = 'inset(0 0 ' + percentage + '% 0)';
+  isHeaderVisible() {
+    // TODO check if header is visible for mobhile
+
+    return true;
+  }
+
+  updateClipPath(percentage) {
+    // var bounds = this.spacer.getBoundingClientRect();
+    // var percentage = ((Math.abs(bounds.y) * 100) / bounds.height) * 2;
+
+    const clipPath = `inset(0 0 ${percentage}% 0)`;
 
     this.root.style.clipPath = clipPath;
     this.isUpdating = false;
@@ -49,19 +60,52 @@ class AbsoluteScroller {
     return this.firstChild === element;
   }
 
+  getPercentage(position, topValue) {
+    const offsetTop = this.spacer.offsetTop - topValue;
+    const offsetBottom = offsetTop + this.spacer.offsetHeight;
+
+    let percentage;
+
+    if (offsetBottom >= position) {
+      const step = (offsetBottom - offsetTop) / 100;
+
+      percentage = (position - offsetTop) / step;
+
+      if (isNaN(percentage)) percentage = 0;
+    } else {
+      percentage = 100;
+    }
+    return percentage;
+  }
+
+  isOutOfViewport(percentage) {
+    return percentage >= this.maxPercentage;
+    // return percentage > this.maxPercentage;
+  }
+
   setStickyPosition() {
-    const headerHeight = document.querySelector('header')?.offsetHeight || 0;
+    const headerHeight = this.getHeaderHeight();
     const scrollPosition = window.scrollY;
     const viewPortOverflow = this.root.offsetHeight - window.innerHeight;
     const scrollThreshold = viewPortOverflow > 0 ? this.offsetBottom : this.offsetBottom - headerHeight;
+    const topValue = viewPortOverflow > 0 ? -viewPortOverflow : this.isFirstChild(this.root) ? 0 : headerHeight;
+    const percentage = this.getPercentage(scrollPosition, topValue);
 
-    if (scrollPosition > scrollThreshold - window.innerHeight) {
-      const topValue = viewPortOverflow > 0 ? -viewPortOverflow : this.isFirstChild(this.root) ? 0 : headerHeight;
+    console.group();
+    console.log(this.root);
+    console.log('AbsoluteScroller ~ setStickyPosition ~ percentage', percentage);
+    console.groupEnd();
 
-      this.root.classList.add(State.STICKY);
+    if (!this.isOutOfViewport(percentage) && scrollPosition > scrollThreshold - window.innerHeight) {
+      if (!this.spacer.style.height) {
+        this.spacer.style.height = this.root.clientHeight + 'px';
+      }
+
       this.root.style.top = topValue + 'px';
+      this.root.classList.remove(State.OFF_SCREEN);
+      this.root.classList.add(State.STICKY);
 
-      this.updateClipPath(scrollPosition, topValue);
+      this.updateClipPath(percentage);
     } else {
       this.disableStickyness();
     }
@@ -69,8 +113,10 @@ class AbsoluteScroller {
 
   disableStickyness() {
     this.root.classList.remove(State.STICKY);
+    this.root.classList.add(State.OFF_SCREEN);
     this.root.style.top = '';
     this.root.style.clipPath = '';
+    this.spacer.style.height = '';
     this.isUpdating = false;
   }
 
@@ -83,12 +129,16 @@ class AbsoluteScroller {
 
     this.spacer.classList.add('absolute-scroller__spacer');
 
-    this.root.style.height = this.spacer.style.height = this.root.clientHeight + 'px';
-    this.root.style.width = this.spacer.style.width = this.root.clientWidth + 'px';
+    this.setDimensions();
 
     parent.insertBefore(this.spacer, this.root.nextSibling);
 
     this.root.dataset.absoluteScroller = true;
+  }
+
+  setDimensions() {
+    this.root.style.height = this.root.clientHeight + 'px';
+    this.root.style.width = this.spacer.style.width = this.root.clientWidth + 'px';
   }
 
   static init() {
