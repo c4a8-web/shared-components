e48c1aca8e0d98657b5db3096e78d8138e63d733
Michael Ehrich <michael.ehrich@krekeny.com>
2023-05-24 17:15:35 +0200

    data(company): adds company data

---
+++ .storybook/data/company.js
@@ -0,0 +1,86 @@
+const Company = {
+  bgColor: 'var(--color-blue-medium)',
+  name: 'glueckkanja-gab AG',
+  street: 'Kaiserstraße 39',
+  postalCode: 63065,
+  city: 'Offenbach',
+  country: 'Germany',
+  number: '+49 69 4005520',
+  mail: 'info@glueckkanja-gab.com',
+  introduction:
+    'Cutting Edge Microsoft Partner in Germany for Microsoft 365, Azure and Cloud Security.<br /><br /><br />',
+  copyright: '2022 glueckkanja-gab AG. All rights reserved.',
+  kununuUrl: 'https://www.kununu.com/de/glueck-kanja-consulting',
+  isoUrl: '/documents/general/gkgab-ISO27001Certificate-de.pdf',
+  logos: [
+    {
+      src: '/logos/gkgab-Logo-white-vektor.svg',
+      alt: 'glueckkanja-gab Logo',
+      url: 'index.html',
+    },
+    {
+      src: '/v1623133806/logos/primepulse-endorsement-white.svg',
+      alt: 'glueckkanja-gab Logo',
+      url: 'https://www.primepulse.de/',
+      target: '_blank',
+      classes: 'offset-lg-6',
+    },
+  ],
+  socials: [
+    {
+      icon: 'fa-twitter',
+      url: 'https://twitter.com/glueckkanjagab',
+      title: 'Twitter',
+    },
+    {
+      icon: 'fa-youtube',
+      url: 'https://www.youtube.com/user/glueckkanja',
+      title: 'Youtube',
+    },
+    {
+      icon: 'fa-linkedin',
+      url: 'https://www.linkedin.com/company/glueckkanja-gab',
+      title: 'LinkedIn',
+    },
+    {
+      icon: 'fa-xing',
+      url: 'https://www.xing.com/pages/glueckkanja-gab',
+      title: 'Xing',
+    },
+    {
+      icon: 'fa-github',
+      url: 'https://github.com/glueckkanja-gab',
+      title: 'Github',
+    },
+  ],
+  links: [
+    {
+      title: 'Datenschutz',
+      url: '/de/datenschutz',
+    },
+    {
+      title: 'Impressum',
+      url: '/de/impressum',
+    },
+    {
+      title: 'No Cookies',
+      url: '/de/cookies',
+    },
+  ],
+  linksEn: [
+    {
+      title: 'Privacy',
+      url: '/en/privacy',
+    },
+    {
+      title: 'Imprint',
+      url: '/en/imprint',
+    },
+    {
+      title: 'No Cookies',
+      url: '/en/cookies',
+    },
+  ],
+};
+
+export default Company;
