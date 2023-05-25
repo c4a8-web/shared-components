85c6ac48a7cc0ea4dc3c66520496d1827be4c5c8
Adnan <adnan.ahmed@glueckkanja-gab.com>
2023-05-25 11:45:35 +0200

    feat(analytics): adblock baiter

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -1,9 +1,39 @@
+import Events from './events.js';
+
+const handleAdBlockerError = function (error) {
+  console.error('This issue is propably caused by an AdBlocker', error);
+
+  const customEvent = new CustomEvent(Events.AD_BLOCK_INFO, {});
+
+  document.dispatchEvent(customEvent);
+  console.group();
+  console.log(document);
+  console.log(customEvent);
+  console.groupEnd();
+};
+
 class AdBlockerBait {
   static rootSelector = '';
   static instances = [];
 
   constructor() {
     console.debug('If this Class can be loaded an AdBlocker is propably not active');
+    this.bait();
+  }
+  bait() {
+    fetch('https://www.google-analytics.com/collect')
+      .then((response) => {
+        if (response.status === 200) {
+          console.log('No adblock probably');
+        } else {
+          console.log('Probably adblock');
+          handleAdBlockerError(response);
+        }
+      })
+      .catch((error) => {
+        console.log('error fetching resources:', error);
+        handleAdBlockerError(error);
+      });
   }
 
   static init() {
