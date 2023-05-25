93096b92b74834883caf3a431625467cb5a3d539
Adnan <adnan.ahmed@glueckkanja-gab.com>
2023-05-25 13:59:29 +0200

    feat(analytics): refactor

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -1,15 +1,11 @@
 import Events from './events.js';
 
 const handleAdBlockerError = function (error) {
-  console.error('This issue is propably caused by an AdBlocker', error);
+  console.error('This issue is probably caused by an AdBlocker', error);
 
   const customEvent = new CustomEvent(Events.AD_BLOCK_INFO, {});
 
   document.dispatchEvent(customEvent);
-  console.group();
-  console.log(document);
-  console.log(customEvent);
-  console.groupEnd();
 };
 
 class AdBlockerBait {
@@ -17,21 +13,20 @@ class AdBlockerBait {
   static instances = [];
 
   constructor() {
-    console.debug('If this Class can be loaded an AdBlocker is propably not active');
     this.bait();
   }
   bait() {
     fetch('https://www.google-analytics.com/collect')
       .then((response) => {
         if (response.status === 200) {
-          console.log('No adblock probably');
+          console.debug('If this Class can be loaded an AdBlocker is probably not active');
         } else {
-          console.log('Probably adblock');
+          console.debug('If this Class can be loaded an AdBlocker is probably active');
           handleAdBlockerError(response);
         }
       })
       .catch((error) => {
-        console.log('error fetching resources:', error);
+        console.debug('If this Class can be loaded an AdBlocker is probably active');
         handleAdBlockerError(error);
       });
   }
