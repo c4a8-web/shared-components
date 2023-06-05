9fc9699ed4dda1fd0e3fa32d101c51ef03707b7e
Adnan <adnan.ahmed@glueckkanja-gab.com>
2023-06-05 13:30:22 +0200

    feat(quotes): Different text sizes and centered Infos

--- stories/components/quotes.stories.js
+++ stories/components/quotes.stories.js
@@ -84,13 +84,25 @@ NoImage.args = {
   quotes: {
     items: [
       {
-        text: 'Mit ‚My Workplace‘ und den darin enthaltenen Microsoft Technologien haben alle unsere Standorte heute die gleichen State-of-the-Art Modern-Workplace Lösungen im Einsatz.',
+        text: 'Lorem ipsum dolor sit amet, consetetur sadipscing',
         name: 'Ronnie Kolk',
         position: 'Projektmanager',
         company: 'Pöppelmann GmbH & Co.KG',
       },
       {
-        text: 'Mit ‚My Workplace‘ und den darin enthaltenen Microsoft Technologien haben alle unsere Standorte heute die gleichen State-of-the-Art Modern-Workplace Lösungen im Einsatz.',
+        text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt',
+        name: 'Ronnie Kolk',
+        position: 'Projektmanager',
+        company: 'Pöppelmann GmbH & Co.KG',
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et',
+        name: 'Ronnie Kolk',
+        position: 'Projektmanager',
+        company: 'Pöppelmann GmbH & Co.KG',
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur',
         name: 'Ronnie Kolk',
         position: 'Projektmanager',
         company: 'Pöppelmann GmbH & Co.KG',
