16d5c471f0550a5c682074ed91fe21c920708295
Michael Ehrich <michael.ehrich@krekeny.com>
2023-06-06 12:31:22 +0200

    fix(utility-animation): only handle animations once

--- assets/js/utility-animation.js
+++ assets/js/utility-animation.js
@@ -7,64 +7,79 @@ class UtilityAnimation {
   constructor(root) {
     this.root = root;
     this.count = 1;
-    this.selector = '[data-utility-animation-step="1"]';
+    this.selector = this.getCurrentSelector();
 
-    this.parentNode = this.root.parentNode.querySelector(this.selector);
-    this.currentElement = this.root.querySelectorAll(this.selector);
+    this.currentElements = this.root.querySelectorAll(this.selector);
 
-    if (!this.currentElement[0].isEqualNode(this.parentNode)) {
-      this.currentElement = this.mergeNodes(this.parentNode, this.currentElement);
+    if (this.root.matches(this.selector)) {
+      this.currentElements = this.mergeNodes(this.root, this.currentElements);
     }
 
+    this.resetAnimation();
     this.initialize();
+  }
+
+  resetAnimation() {
+    this.root.querySelectorAll(`.${State.IS_STARTING}`).forEach((element) => {
+      element.classList.remove(State.IS_STARTING);
+    });
   }
 
   mergeNodes(Node, NodeList) {
     const array = Array.from(NodeList);
+
     array.unshift(Node);
+
     return array;
+  }
+
+  getCurrentSelector() {
+    return `[data-utility-animation-step="${this.count}"]`;
   }
 
   updateCurrentElement(event) {
     const newArray = [];
-    this.currentElement.forEach((element) => {
-      if (event.srcElement === element) {
-      } else {
+
+    this.currentElements.forEach((element) => {
+      if (event.srcElement !== element) {
         newArray.push(element);
       }
     });
-    this.currentElement = newArray;
+
+    this.currentElements = newArray;
   }
 
   handleAnimationEnd(event) {
     this.updateCurrentElement(event);
-    if (this.currentElement.length > 0) return;
+
+    if (this.currentElements.length > 0) return;
 
     this.count++;
-    const searchQuery = `[data-utility-animation-step="${this.count}"]`;
-    this.currentElement = this.root.querySelectorAll(searchQuery);
-    this.currentElement = Array.from(this.currentElement);
 
-    if (this.currentElement !== null && this.currentElement.length > 0) {
-      this.toggleState(this.currentElement);
+    this.currentElements = this.root.querySelectorAll(this.getCurrentSelector());
+
+    if (this.currentElements !== null && this.currentElements.length > 0) {
+      this.startStepAnimation(this.currentElements);
     }
   }
 
-  toggleState(element) {
-    element.forEach((e) => {
-      e.classList.toggle(State.IS_STARTING);
+  startStepAnimation(elements) {
+    elements.forEach((element) => {
+      element.classList.add(State.IS_STARTING);
     });
   }
 
   initialize() {
-    this.root.addEventListener('animationend', (e) => {
-      this.handleAnimationEnd(e);
+    this.root.addEventListener('animationend', (event) => {
+      this.handleAnimationEnd(event);
     });
-    this.toggleState(this.currentElement);
+
+    this.startStepAnimation(this.currentElements);
   }
 
   static init() {
     this.instances = [];
+
     [].forEach.call(document.querySelectorAll(this.rootSelector), (element) => {
       this.instances.push(new this(element));
     });
