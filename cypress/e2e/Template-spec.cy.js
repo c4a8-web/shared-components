7cc9273a7152d7823ac66a2f83c38800e2dad239
Michael Ehrich <michael.ehrich@krekeny.com>
2023-06-19 12:26:17 +0200

    chore(cypress): test

--- cypress/e2e/Template-spec.cy.js
+++ cypress/e2e/Template-spec.cy.js
@@ -6,35 +6,35 @@ describe('Template Test Success!', () => {
 });
 
 // Fail Template Test
-// describe('Template Test Fail!', () => {
-//   it('Template Fail Test!', () => {
-//     expect(true).to.equal(false);
-//   });
-// });
+describe('Template Test Fail!', () => {
+  it('Template Fail Test!', () => {
+    expect(true).to.equal(false);
+  });
+});
 
-// describe('Template Example Test!', () => {
-//   it('Template Example Test!', () => {
-//     let image;
+describe('Template Example Test!', () => {
+  it('Template Example Test!', () => {
+    let image;
 
-//     cy.visit('http://localhost:6006/iframe.html?args=&id=components-accordion--default&viewMode=story');
+    cy.visit('http://localhost:6006/iframe.html?args=&id=components-accordion--default&viewMode=story');
 
-//     // Clicks on all Buttons
-//     cy.get('.accordion__btn').each((item, index) => {
-//       if (index != 0) {
-//         cy.wrap(item).click();
-//         cy.wait(500);
-//       }
+    // Clicks on all Buttons
+    cy.get('.accordion__btn').each((item, index) => {
+      if (index != 0) {
+        cy.wrap(item).click();
+        cy.wait(500);
+      }
 
-//       // Checks if Accordion Card Text is not empty
-//       cy.get('.accordion__text').should('not.be.empty');
+      // Checks if Accordion Card Text is not empty
+      cy.get('.accordion__text').should('not.be.empty');
 
-//       // Checks for no duplicate Images
-//       cy.get('.v-img')
-//         .invoke('attr', 'src')
-//         .then((src) => {
-//           expect(false).to.equal(src === image);
-//           image = src;
-//         });
-//     });
-//   });
-// });
+      // Checks for no duplicate Images
+      cy.get('.v-img')
+        .invoke('attr', 'src')
+        .then((src) => {
+          expect(false).to.equal(src === image);
+          image = src;
+        });
+    });
+  });
+});
--- scripts/generateTests.mjs
+++ scripts/generateTests.mjs
@@ -1,9 +1,14 @@
 import { execa } from 'execa';
 import { exec } from 'child_process';
+import { promisify } from 'util';
 import { platform } from 'os';
 import { get } from 'http';
 
+const execPromise = promisify(exec);
+
 function checkLocalhost(callback) {
+  console.log('Checking localhost...');
+
   const options = {
     host: 'localhost',
     port: 6006,
@@ -30,7 +35,7 @@ async function closeStorybook() {
   const isWindows = operatingSystem.includes('win');
   const command = isWindows ? 'taskkill /IM node.exe /F' : 'pkill -f start-storybook';
 
-  await execa(command, {
+  await execPromise(command, {
     shell: true,
     stdio: 'inherit',
     windowsVerbatimArguments: isWindows,
@@ -48,20 +53,22 @@ async function main() {
   try {
     console.log('Running npm run storybook...');
 
-    exec('npm run hello', (error, stdout, stderr) => {
-      if (error) {
-        console.error(`exec error: ${error}`);
-        return;
-      }
-      console.log(`stdout: ${stdout}`);
-      console.error(`stderr: ${stderr}`);
-    });
+    // exec('npm run hello', (error, stdout, stderr) => {
+    //   if (error) {
+    //     console.error(`exec error: ${error}`);
+    //     return;
+    //   }
+    //   console.log(`stdout: ${stdout}`);
+    //   console.error(`stderr: ${stderr}`);
+    // });
 
-    return;
+    const storybookProcess = execPromise(`npm run storybook`);
 
-    const storybookProcess = execa('npm run storybook');
-    storybookProcess.stdout.pipe(process.stdout);
-    storybookProcess.stderr.pipe(process.stderr);
+    // storybookProcess.stdout.pipe(process.stdout);
+    // storybookProcess.stderr.pipe(process.stderr);
+
+    // console.log('🚀 ~ file: generateTests.mjs:65 ~ main ~ stderr:', stderr);
+    // console.log('🚀 ~ file: generateTests.mjs:65 ~ main ~ stdout:', stdout);
 
     console.log('Waiting for localhost:6006...');
     await new Promise((resolve) => {
@@ -78,19 +85,52 @@ async function main() {
     console.log('localhost:6006 is accessible.');
 
     console.log('Running npm run cypress:test...');
-    const cypressProcess = execa('npm run cypress:test', { stdio: 'inherit' });
-
-    cypressProcess
+    const cypressProcess = execPromise('npm run cypress:test')
       .then(() => {
         console.log('Tests completed. Terminating script...');
-        storybookProcess.kill();
+        // storybookProcess.kill();
         closeStorybook();
       })
       .catch((err) => {
         console.error('Tests failed', err);
-        storybookProcess.kill();
+        // storybookProcess.kill();
         closeStorybook();
       });
+
+    return;
+
+    // const storybookProcess = execa('npm run storybook');
+    // storybookProcess.stdout.pipe(process.stdout);
+    // storybookProcess.stderr.pipe(process.stderr);
+
+    // console.log('Waiting for localhost:6006...');
+    // await new Promise((resolve) => {
+    //   const interval = setInterval(() => {
+    //     checkLocalhost((isAccessible) => {
+    //       if (isAccessible) {
+    //         clearInterval(interval);
+    //         resolve();
+    //       }
+    //     });
+    //   }, 1000);
+    // });
+
+    console.log('localhost:6006 is accessible.');
+
+    console.log('Running npm run cypress:test...');
+    // const cypressProcess = execa('npm run cypress:test', { stdio: 'inherit' });
+
+    // cypressProcess
+    //   .then(() => {
+    //     console.log('Tests completed. Terminating script...');
+    //     storybookProcess.kill();
+    //     closeStorybook();
+    //   })
+    //   .catch((err) => {
+    //     console.error('Tests failed', err);
+    //     storybookProcess.kill();
+    //     closeStorybook();
+    //   });
   } catch (error) {
     console.error('An error occurred:', error);
 
