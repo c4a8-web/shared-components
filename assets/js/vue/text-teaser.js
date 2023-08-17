dde9ff762c5e6ed32af1323f76e58addc45af1fa
Adnan <adnan.ahmed@glueckkanja-gab.com>
2023-08-17 10:56:51 +0200

    feat(text-teaser): updated wip

--- assets/js/vue/text-teaser.js
+++ assets/js/vue/text-teaser.js
@@ -2,12 +2,7 @@ export default {
   tagName: 'text-teaser',
   computed: {
     classList() {
-      return [
-        'text-teaser',
-        this.logo ? 'overflow-hidden' : 'text-teaser--bg utility-animation',
-        this.spacing ? this.spacing : '',
-        'vue-component',
-      ];
+      return ['text-teaser utility-animation', this.spacing ? this.spacing : '', 'vue-component'];
     },
     style() {
       return [
@@ -15,37 +10,27 @@ export default {
         this.bgColor ? `background-color: ${this.bgColor};` : '',
       ];
     },
-    containerList() {
-      return ['container', this.logo ? 'space-bottom-2' : ''];
-    },
     rowBackgroundClass() {
-      return this.logo ? ['position-relative'] : ['row', this.background ? 'justify-content-center' : ''];
+      return ['row', this.background ? 'justify-content-center' : ''];
     },
     colBackgroundClass() {
-      return this.logo
-        ? ['bg-light text-center rounded-lg p-4 p-md-7']
-        : [
-            this.background
-              ? 'col-lg-6 py-11 d-flex text-center justify-content-center align-items-center flex-wrap font-size-4 font-weight-light'
-              : 'col-lg-7 offset-lg-1 pt-8',
-          ];
+      return [
+        this.background
+          ? 'col-lg-6 py-11 d-flex text-center justify-content-center align-items-center flex-wrap font-size-4 font-weight-light'
+          : 'col-lg-7 offset-lg-1 pt-8',
+      ];
     },
     ctaListClass() {
-      return this.logo
-        ? ['w-lg-50 mx-lg-auto pb-11 pb-lg-0 mb-6 mb-sm-0']
-        : ['pt-4 pt-lg-6 w-100 w-md-auto fade-in-bottom'];
+      return ['pt-4 pt-lg-6 w-100 w-md-auto fade-in-bottom'];
     },
-    ctaListRefactor() {
-      const invalidKeyText = 'ctaText';
-      const invalidKeyHref = 'ctaHref';
-
+    ctaListNormalize() {
       const fixedArr = this.ctaList.map((obj) => {
         const newObj = {};
 
         for (const key in obj) {
-          if (key === invalidKeyText) {
+          if (key === 'ctaText') {
             newObj['text'] = obj[key];
-          } else if (key === invalidKeyHref) {
+          } else if (key === 'ctaHref') {
             newObj['href'] = obj[key];
           } else {
             newObj[key] = obj[key];
@@ -79,35 +64,22 @@ export default {
   },
   template: `
     <div :class="classList" :style="style">
-      <div :class="containerList">
+      <div class="container">
         <div :class="rowBackgroundClass">
           <div :class="colBackgroundClass">
-            <template v-if="logo">
-              <div class="mb-4">
-                <figure class="max-w-11rem max-w-md-13rem mx-auto">
-                  <v-img :img="logo.img" :cloudinary="logo.cloudinary" :alt="logo.alt" />
-                </figure>
-              </div>
+            <div class="mb-4 w-100" v-if="logo">
+              <figure class="max-w-11rem max-w-md-13rem mx-auto">
+                <v-img v-bind="logo" />
+              </figure>
+            </div>
 
-              <div class="w-md-80 w-lg-50 mx-md-auto mb-6">
-                <blockquote class="lead text-dark">{{ copy }}</blockquote>
-              </div>
-            </template>
-            <template v-else>
-              <div class="text-teaser__copy fade-in-bottom" data-utility-animation-step="1">{{ copy }}</div>
-            </template>
+            <div class="text-teaser__copy fade-in-bottom" data-utility-animation-step="1">{{ copy }}</div>
 
             <div :class="ctaListClass" data-utility-animation-step="1" v-if="ctaList">
-              <template v-for="entry in ctaListRefactor">
+              <template v-for="entry in ctaListNormalize">
                 <cta v-bind="entry" />
               </template>
             </div>
-
-            <figure class="max-w-15rem w-100 position-absolute bottom-0 left-0" v-if="logo">
-              <div class="mb-n7 ml-n7">
-                <v-img :img="logo.background" :cloudinary="logo.cloudinary" :alt="logo.alt" />
-              </div>
-            </figure>
 
           </div>
         </div>
--- stories/components/textTeaser.stories.js
+++ stories/components/textTeaser.stories.js
@@ -25,9 +25,43 @@ TextTeaser.args = {
   ],
 };
 
+export const TextTeaserNoBackground = Template.bind({});
+
+TextTeaserNoBackground.args = {
+  copy: 'Als Advanced Specialization Partner können Sie über uns am Azure Migration Program teilnehmen und von attraktiven Rabatten und Azure Credits profitieren.',
+  list: [
+    {
+      ctaText: 'Sprechen Sie uns an',
+      ctaHref: 'mailto:leads@glueckkanja-gab.com',
+    },
+  ],
+};
+
 export const PartnerQuote = Template.bind({});
 
 PartnerQuote.args = {
+  bgColor: 'var(--color-black-4)',
+  background: getAssetPath('svg/shapes/shape-5.svg'),
+  copy: 'Wir ermöglichen es Unternehmen, schnell und sicher in die Cloud zu wechseln, indem wir eine beispiellose Cloud-Transformations- und Optimierungstechnologie bereitstellen.',
+  list: [
+    {
+      ctaText: 'Erfahren Sie mehr zu Txture',
+      ctaHref: 'javascript:void',
+      link: false,
+      target: '_blank',
+    },
+  ],
+  logo: {
+    alt: 'logo-txture',
+    img: getAssetPath('svg/logos/gkgab-RADIUSaaS.svg'),
+    cloudinary: false,
+    alt: 'Lorem Ipsum',
+  },
+};
+
+export const PartnerQuoteNoBackground = Template.bind({});
+
+PartnerQuoteNoBackground.args = {
   copy: 'Wir ermöglichen es Unternehmen, schnell und sicher in die Cloud zu wechseln, indem wir eine beispiellose Cloud-Transformations- und Optimierungstechnologie bereitstellen.',
   list: [
     {
@@ -40,7 +74,6 @@ PartnerQuote.args = {
   logo: {
     alt: 'logo-txture',
     img: getAssetPath('svg/logos/logo-txture.svg'),
-    background: getAssetPath('svg/shapes/bg-abstract-shapes.svg'),
     cloudinary: false,
     alt: 'Lorem Ipsum',
   },
