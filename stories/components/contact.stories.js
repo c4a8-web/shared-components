acdfc238428f963d9fe2c4e16f9a9ecbfe5476dc
Michael Ehrich <michael.ehrich@krekeny.com>
2023-10-02 15:28:28 +0200

    feat(contact): adds ability to override the colors

--- stories/components/contact.stories.js
+++ stories/components/contact.stories.js
@@ -228,4 +228,98 @@ WithQuote.args = {
     },
   },
 };
+
+export const WithQuoteBgColor = Template.bind({});
+
+WithQuoteBgColor.args = {
+  bgColor: 'var(--color-blue-jeans)',
+  color: 'var(--color-copy)',
+  boxBgColor: 'var(--color-gigas)',
+  boxColor: 'var(--color-white)',
+  quote: true,
+  infos: {
+    headline: 'Jetzt Gesprächstermin vereinbaren',
+    level: 'h2',
+    person: {
+      image: '/people/people-jan-geisbauer-csoc.jpg',
+      mail: 'sales@glueckkanja-gab.com',
+      number: '+49694005520',
+      cloudinary: true,
+      alt: 'Quotation Svg/Picture Platzhalter',
+      name: 'Platzhalter',
+      quotee: 'Jan Geisbauer',
+      quoteeTitle: 'Cyber Security Lead',
+      quote:
+        'Bei den meisten unserer Emergency-Einsätze stellen wir immer wieder fest, dass die IT nicht gut genug auf Angriffe vorbereitet war. Ein pro-aktiver Security Check ist deshalb eine effiziente Investition in mehr Sicherheit zur Reduzierung der Ausfallzeiten.',
+      detailsHeader: 'Wir freuen uns darauf, von Ihnen zu hören',
+      details: [
+        {
+          text: '+49 69 4005520',
+          href: 'tel:+49 69 4005520',
+          details: 'lorem ipsum dolor sit amet lore',
+          icon: 'site/phone',
+        },
+        {
+          text: 'kontakt@glueckkanja-gab.com',
+          href: 'mailto:kontakt@glueckkanja-gab.com',
+          icon: 'site/mail',
+        },
+      ],
+    },
+    form: {
+      ctaText: 'Absenden',
+      cta: {
+        skin: 'primary is-light',
+      },
+      method: 'post',
+      action: '/',
+      fields: [
+        {
+          label: 'Name',
+          type: 'text',
+          id: 'name',
+          required: true,
+          requiredMsg: 'Bitte geben Sie ihren Namen an.',
+        },
+        {
+          label: 'Unternehmen',
+          type: 'text',
+          id: 'company',
+          required: true,
+          requiredMsg: 'Bitte geben Sie ihr Unternehmen an',
+        },
+        {
+          label: 'Email-Adresse',
+          type: 'email',
+          id: 'email',
+          required: true,
+          requiredMsg: 'Bitte geben Sie ihre E-Mail-Adresse ein',
+        },
+        {
+          label: 'Nachricht',
+          type: 'textarea',
+          id: 'message',
+          required: false,
+          requiredMsg: 'Bitte ausfüllen',
+        },
+        {
+          label:
+            'Ich bin mit der Speicherung meiner Daten einverstanden, die Datenschutzerklärung habe ich gelesen und akzeptiert. Meine Einwilligung kann ich jederzeit widerrufen.',
+          type: 'checkbox',
+          id: 'checkbox1',
+        },
+        {
+          type: 'hidden',
+          id: 'inbox_key',
+          value: 'gkab-contact-form',
+        },
+        {
+          type: 'hidden',
+          id: '_gotcha',
+        },
+      ],
+    },
+  },
+};
+
 // TODO add shape variant without form
