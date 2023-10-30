70c6f63592e6662924b02b26d9f18e02439c7a38
Michael Ehrich <michael.ehrich@krekeny.com>
2023-10-30 12:57:26 +0100

    docs(event): adds docs

--- stories/components/event.stories.js
+++ stories/components/event.stories.js
@@ -7,6 +7,61 @@ const options = getTitle({
 
 export default {
   ...options,
+  argTypes: {
+    title: {
+      description: 'The Title',
+      type: 'string',
+    },
+    date: {
+      description: 'The date of the element in YYYY-MM-DD or in DD.-DD.MM.YYYY format',
+      type: 'string',
+    },
+    category: {
+      description: 'The category',
+      type: 'string',
+    },
+    text: {
+      description: 'The text of the element',
+      type: 'string',
+    },
+    image: {
+      description: 'The image data object',
+      type: 'object',
+    },
+    bgColor: {
+      description: 'The background color of the date block',
+      options: ['', 'var(--color-yellow)', 'var(--color-primary)'],
+      control: {
+        type: 'select',
+      },
+    },
+    color: {
+      description: 'The text color of the date block',
+      options: ['', 'var(--color-copy)', 'var(--color-copy-light)', 'var(--color-yellow-medium)'],
+      control: {
+        type: 'select',
+      },
+    },
+    timeColor: {
+      description: 'The text color of the category and the time text',
+      options: ['', 'var(--color-copy)', 'var(--color-copy-light)', 'var(--color-yellow-medium)'],
+      control: {
+        type: 'select',
+      },
+    },
+    time: {
+      description: 'The time of the element',
+      type: 'string',
+    },
+    url: {
+      description: 'The url of the element',
+      type: 'string',
+    },
+    classes: {
+      description: 'Additional Classes of the element',
+      type: 'string',
+    },
+  },
 };
 
 const Template = (args) => createComponent(args, component);
