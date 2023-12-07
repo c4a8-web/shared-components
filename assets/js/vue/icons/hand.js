3253f2d1f221c8131049155215f25c7f5f9cbe89
Adnan <adnan.ahmed@glueckkanja-gab.com>
2023-12-07 13:19:15 +0100

    feat(hand): updates to animation

--- assets/js/vue/icons/hand.js
+++ assets/js/vue/icons/hand.js
@@ -1,6 +1,7 @@
-const iconHandStepTime = 'indefinite';
-const iconHandDuration = '1.5s';
-const iconHandRepetition = '3';
+const iconHandStepTime = '0s';
+const iconHandDuration = '1.2s';
+const iconHandRepetition = '2';
+const iconHandPause = '3s';
 
 export default {
   tagName: 'hand',
@@ -18,7 +19,7 @@ export default {
   },
   template: `
   <svg v-bind="settings" :style="style" viewBox="0 0 308 304" fill="none">
-    <g id="hand-outline">
+    <g class="hand-outline" id="hand-outline">
       <path id="hand-outline-outter" d="M50.5707 135.135C42.6425 127.207 29.4289 127.207 20.1793 135.135C12.2511 143.063 12.2511 156.277 20.1793 165.527L133.816 279.164C147.03 292.378 165.529 300.306 185.35 300.306C205.17 300.306 223.669 292.378 236.883 279.164L265.953 250.094C285.774 230.274 295.023 201.204 289.738 173.455L271.239 82.281C269.917 73.0314 263.31 66.4246 254.061 65.1032C242.168 62.4605 228.955 71.7101 227.633 83.6024L222.348 127.207L124.567 29.4264C116.639 21.4982 103.425 21.4982 94.1758 29.4264C86.2476 37.3546 86.2476 50.5682 94.1758 59.8178L80.9621 45.2828C73.0339 37.3546 59.8203 37.3546 50.5707 45.2828C42.6425 53.211 42.6425 66.4246 50.5707 75.6742L65.1057 90.2092C57.1775 82.281 43.9639 82.281 34.7143 90.2092C26.7861 98.1374 26.7861 111.351 34.7143 120.601L50.5707 135.135Z" :stroke="stroke" stroke-width="6" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
       <path id="hand-outline-inner-finger" d="M94.1547 59.7668L153.616 119.228" :stroke="stroke" stroke-width="6" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
       <path id="hand-outline-inner-finger" d="M65.1072 90.2095L125.891 149.671" :stroke="stroke" stroke-width="6" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
@@ -35,19 +36,35 @@ export default {
       <path id="outter-line-bottom" d="M3 247.452C3 276.522 26.7846 300.307 55.8546 300.307" :stroke="stroke" stroke-width="6" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
     </g>
 
-    <animateTransform class="js-text-animation__step-1" href="#hand-outline" attributeName="transform" type="rotate"
-      values="-17 187.5 192; 22 187.5 192; -17 187.5 192"
-      begin="${iconHandStepTime}" dur="${iconHandDuration}" repeatCount="${iconHandRepetition}"
+    <animateTransform id="pause-hand-rotation" href="#hand-outline" attributeName="transform" type="rotate"
+      values="0 187.5 192"
+      begin="hand-rotation.end+0s" dur="${iconHandPause}"
     />
 
-    <animate class="js-text-animation__step-1" href="#inner-lines" attributeName="opacity"
+    <animateTransform id="hand-rotation" href="#hand-outline" attributeName="transform" type="rotate"
+      values="0 187.5 192; -17 187.5 192; 22 187.5 192; 0 187.5 192"
+      begin="${iconHandStepTime};pause-hand-rotation.end+0s" dur="${iconHandDuration}"
+      repeatCount="${iconHandRepetition}"
+    />
+
+    <animate id="pause-inner-lines-opacity" href="#inner-lines" attributeName="opacity"
+      values="0.9"
+      begin="inner-lines-opacity.end+0s" dur="${iconHandPause}"
+    />
+
+    <animate id="inner-lines-opacity" href="#inner-lines" attributeName="opacity"
       values="0.9; 0.05; 0.9"
-      begin="${iconHandStepTime}" dur="${iconHandDuration}" repeatCount="${iconHandRepetition}"
+      begin="${iconHandStepTime}; pause-inner-lines-opacity.end+0s" dur="${iconHandDuration}" repeatCount="${iconHandRepetition}"
     />
 
-    <animate class="js-text-animation__step-1" href="#outter-lines" attributeName="opacity"
+    <animate id="pause-outter-lines-opacity" href="#outter-lines" attributeName="opacity"
+      values="0.9"
+      begin="outter-lines-opacity.end+0s" dur="${iconHandPause}"
+    />
+
+    <animate id="outter-lines-opacity" href="#outter-lines" attributeName="opacity"
       values="0.05; 0.9; 0.05"
-      begin="${iconHandStepTime}" dur="${iconHandDuration}" repeatCount="${iconHandRepetition}"
+      begin="${iconHandStepTime}; pause-outter-lines-opacity.end+0s" dur="${iconHandDuration}" repeatCount="${iconHandRepetition}"
     />
   </svg>
   `,
