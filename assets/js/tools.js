293fed56ee4b91f4d5a53845603bcef20203a5fa
Michael Ehrich <michael.ehrich@krekeny.com>
2023-12-21 14:35:35 +0100

    feat(tools): adjusts toSize function to show value behind comma

--- assets/js/tools.js
+++ assets/js/tools.js
@@ -150,7 +150,7 @@ class Tools {
 
     const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
 
-    return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + units[i];
+    return (bytes / Math.pow(1024, i)).toFixed(1) + ' ' + units[i];
   }
 
   static camalCaseToSnakeCase(camalCase) {
