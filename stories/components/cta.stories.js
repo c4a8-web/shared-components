54611e5e2c29eef8aa0e8a9bad0b6b0db9409bba
Adnan <adnan.ahmed@glueckkanja-gab.com>
2024-02-13 14:17:22 +0100

    feat(cta): reverts changes

--- stories/components/cta.stories.js
+++ stories/components/cta.stories.js
@@ -159,15 +159,6 @@ PrimaryLinkReversed.args = {
   reversed: true,
 };
 
-export const PrimaryLinkExternal = Template.bind({});
-
-PrimaryLinkExternal.args = {
-  text: 'Primary Link',
-  href: 'javascript:void(0)',
-  link: true,
-  external: true,
-};
-
 export const PrimaryButtonLoading = Template.bind({});
 
 PrimaryButtonLoading.args = {
