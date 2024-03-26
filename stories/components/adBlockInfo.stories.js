cff028386b6733fa6eabe0d260ee48d329b9567b
Michael Ehrich <michael.ehrich@krekeny.com>
2024-03-26 13:18:10 +0100

    chore(stories): updates through migration script

--- stories/components/adBlockInfo.stories.js
+++ stories/components/adBlockInfo.stories.js
@@ -1,34 +1,34 @@
-import { createComponent, getTitle, getDecorators, getArgTypes, createStory } from '../../.storybook/templates';
-// import { includesadblockinfohtml as component } from '../../.storybook/generatedIncludes';
-import adBlockInfoTemplate from '!!raw-loader!./adBlockInfo.html';
-
-const Template = (args) => createComponent(args, adBlockInfoTemplate);
+import { getTitle, getDefaultSettings } from '../../.storybook/templates';
+import component from '!!raw-loader!./adBlockInfo.html';
 
 const options = getTitle({
   title: 'Ad Block Info',
 });
 
-export default getArgTypes({
-  ...options,
-  argTypes: {
-    text: {
-      description: 'The Text of the Ad Block Info',
-      type: 'string',
+export default {
+  ...getDefaultSettings({
+    options,
+    component,
+    argTypes: {
+      text: {
+        description: 'The Text of the Ad Block Info',
+        type: 'string',
+      },
+      show: {
+        description: 'Initializes the Modal with Ad Block Info',
+        type: 'boolean',
+      },
     },
-    show: {
-      description: 'Initializes the Modal with Ad Block Info',
-      type: 'boolean',
-    },
-  },
-});
+  }),
+};
 
 // TODO figure out why create story does not work here
 
-export const CustomText = Template.bind({});
-
-CustomText.args = {
-  show: true,
-  text: '<h3>AdBlocker Info</h3>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+export const CustomText = {
+  args: {
+    show: true,
+    text: '<h3>AdBlocker Info</h3>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+  },
 };
 
 // export const CustomText = createStory(adBlockInfoTemplate, {
@@ -36,7 +36,7 @@ CustomText.args = {
 //     text: 'Lorem Ipsum',
 //     show: true,
 //   },
-// });
+// } };
 
 // export const DefaultText = createStory(adBlockInfoTemplate, {
 //   adBlockInfo: {
