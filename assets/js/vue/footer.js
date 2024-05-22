5c508f45afce05707460ca296c1b019e676ba054
Michael Ehrich <michael.ehrich@krekeny.com>
2024-05-22 14:17:30 +0200

    feat(footer): removes old markup and adds slider styling

--- assets/js/vue/footer.js
+++ assets/js/vue/footer.js
@@ -49,86 +49,6 @@ export default {
   template: `
     <footer :class="classList" :style="style">
       <div class="container">
-        <div class="space-top-2 space-top-lg-3 space-bottom-1">
-          <div class="row justify-content-lg-between">
-            <div class="col-lg-3 ml-lg-auto mb-2 mb-lg-0 footer__contacts">
-
-              <div :class="['mb-6', logo?.classes]" v-for="logo in dataValue.logos">
-                <a
-                  :href="logo.url"
-                  :target="logo.target"
-                  aria-label="Front"
-                  :class="['footer__logos-link d-block pr-6', logo.linkClasses ? logo.linkClasses : 'w-65 w-md-35 w-lg-90']"
-                >
-                  <v-img
-                    cloudinary=true
-                    v-bind="logo"
-                  ></v-img>
-                </a>
-              </div>
-
-              <ul class="footer__locations nav nav-sm nav-x-0 nav-white flex-column" v-for="location in locations">
-                <li class="nav-item">
-                  {{ location.name }}
-                </li>
-                <li class="nav-item" v-if="location.over">{{ location.over }}</li>
-                <li class="nav-item">
-                  {{ location.street }}
-                </li>
-                <li :class="['nav-item footer__address-block', location.postalReversed ? 'is-reversed' : '']">
-                  <span class="footer__postal-code">{{ location.postalCode }}</span>
-                  <span class="footer__city">{{ location.city }}</span>
-                  <span class="footer__country">{{ location.country }}</span>
-                </li>
-                <li class="nav-item" v-if="location.number">
-                  <a class="nav-link media" :href="'tel:' + location.number">
-                    <span class="media">
-                      <span class="streamline-xs streamline-site-phone mr-3 d-flex"><slot name='icon-phone'></slot></span>
-                      <span class="media-body">
-                        {{ location.number }}
-                      </span>
-                    </span>
-                  </a>
-                </li>
-                <li :class="['nav-item', locations.length > 1 ? 'pt-4' : '']" v-if="location.mail">
-                  <a class="nav-link media pt-3" :href="'mailto:' + location.mail">
-                    <span class="media">
-                      <span class="streamline-xs streamline-site-mail mt-1 mr-3 d-flex"><slot name='icon-mail'></slot></span>
-                      <span class="media-body">
-                        {{ location.mail }}
-                      </span>
-                    </span>
-                  </a>
-                </li>
-              </ul>
-            </div>
-
-            <div class="footer__images col-lg-9">
-              <div class="row no-gutters ml-lg-4">
-                <a :href="partner.url" :target="partner.target"  :class="['footer__partner-images col-lg-4 px-lg-5', partner.width ? 'w-lg-100 w-' + partner.width : 'w-100']" v-for="(partner, index) in dataValue.partners" :style="index === dataValue.partners.length-1 ? 'padding-left: 2rem !important;padding-right: 0rem !important;' : ''">
-                  <v-img
-                    cloudinary=true
-                    v-bind="partner"
-                    class="footer__partner-image"
-                  ></v-img>
-                </a>
-              </div>
-
-              <div class="row no-gutters ml-lg-4">
-                <template v-for="(highlight, index) in dataValue.highlights">
-                  <span v-if="highlight.title" class="d-block space-top-2 mb-n7 w-90 w-lg-100 pr-6">{{ highlight.title }}</span>
-                  <a :href="highlight.url" :target="highlight.target" :class="['footer__highlight-link d-block space-top-1', index === 0 ? 'mt-3': '', highlight.classes ? highlight.classes : 'w-90']">
-                    <v-img
-                      cloudinary=true
-                      v-bind="highlight"
-                    ></v-img>
-                  </a>
-                </template>
-              </div>
-            </div>
-          </div>
-        </div>
-
         <div class="footer__content-row row">
           <div class="footer__content col-lg-12">
             <div class="footer__address">
@@ -206,8 +126,9 @@ export default {
                 </template>
               </div>
 
-              <div class="footer__slider">
+              <div class="footer__slider-container">
                 <slider
+                  class="footer__slider"
                   hide-background="true"
                 >
                   <a v-for="(highlight, index) in dataValue.highlights" :href="highlight.url" :target="highlight.target" :class="['footer__highlight-link d-block space-top-1', index === 0 ? 'mt-3': '', highlight.classes ? highlight.classes : 'w-90']">
