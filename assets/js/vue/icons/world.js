3c3faa0021f6d3ae9000924287d4991ca6d0cd5f
Michael Ehrich <michael.ehrich@krekeny.com>
2024-06-05 12:36:00 +0200

    feat(world): adjusts icon

--- assets/js/vue/icons/world.js
+++ assets/js/vue/icons/world.js
@@ -13,17 +13,17 @@ export default {
     color: String,
   },
   template: `
-    <svg v-bind="settings" :style="style" fill="none" viewBox="0 0 22 24">
+    <svg v-bind="settings" :style="style" fill="none" viewBox="0 0 20 20">
       <g clip-path="url(#icon-world-mask)">
-        <path d="M20.583 13.9508C20.5836 15.2135 20.3356 16.4639 19.853 17.6307C19.3704 18.7975 18.6627 19.8578 17.7703 20.7511C16.878 21.6444 15.8184 22.3532 14.6521 22.837C13.4858 23.3208 12.2356 23.5702 10.973 23.5708C8.41074 23.5283 5.96901 22.4755 4.17921 20.6415C2.38941 18.8075 1.3964 16.3409 1.41631 13.7783C1.41836 11.329 2.37927 8.97772 4.09332 7.22801C5.80737 5.47829 8.13832 4.46917 10.5871 4.41668C10.7255 4.41084 10.8638 4.40834 11.0038 4.40834C12.2606 4.40153 13.5063 4.64343 14.6692 5.12012C15.832 5.5968 16.8891 6.29886 17.7795 7.18584C18.6699 8.07282 19.376 9.1272 19.8572 10.2882C20.3383 11.4493 20.585 12.6941 20.583 13.9508Z" :stroke="stroke" stroke-opacity="0.8" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M10.5879 4.41669C5.58789 9.83335 5.58789 16.9 10.5879 23.5667" :stroke="stroke" stroke-opacity="0.8" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M11.4209 4.41669C16.4209 9.83335 16.4209 16.8975 11.4209 23.5642" :stroke="stroke" stroke-opacity="0.8" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M1.41895 13.57H20.5831" :stroke="stroke" stroke-opacity="0.8" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M19.583 9.95083C19.5836 11.2135 19.3356 12.4639 18.853 13.6307C18.3704 14.7975 17.6627 15.8578 16.7703 16.7511C15.878 17.6444 14.8184 18.3532 13.6521 18.837C12.4858 19.3208 11.2356 19.5702 9.97298 19.5708C7.41074 19.5283 4.96901 18.4754 3.17921 16.6415C1.38941 14.8075 0.396399 12.3408 0.416309 9.77833C0.41836 7.32895 1.37927 4.97771 3.09332 3.22799C4.80737 1.47828 7.13832 0.46915 9.58714 0.416661C9.72548 0.410828 9.86381 0.408328 10.0038 0.408328C11.2606 0.401511 12.5063 0.643413 13.6692 1.1201C14.832 1.59679 15.8891 2.29884 16.7795 3.18582C17.6699 4.0728 18.376 5.12718 18.8572 6.28823C19.3383 7.44927 19.585 8.69404 19.583 9.95083Z" :stroke="stroke" stroke-opacity="0.8" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M9.58789 0.416672C4.58789 5.83334 4.58789 12.9 9.58789 19.5667" :stroke="stroke" stroke-opacity="0.8" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M10.4209 0.416672C15.4209 5.83334 15.4209 12.8975 10.4209 19.5642" :stroke="stroke" stroke-opacity="0.8" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M0.418945 9.57001H19.5831" :stroke="stroke" stroke-opacity="0.8" stroke-linecap="round" stroke-linejoin="round"/>
       </g>
       <defs>
-        <clipPath id="icon-world-mask">
-          <rect width="20" height="20" :fill="stroke" transform="translate(1 4)"/>
-        </clipPath>
+      <clipPath id="icon-world-mask">
+        <rect width="20" height="20" :fill="stroke" />
+      </clipPath>
       </defs>
     </svg>
   `,
