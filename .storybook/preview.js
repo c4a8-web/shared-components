b7febef84d8c66e58e96813e1f1e020305ad64f3
Michael Ehrich <michael.ehrich@krekeny.com>
2024-09-18 16:45:51 +0200

    feat(preview): adds wip support for storybook styles loading

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,22 @@
-import "../src/assets/scss/index.scss";
+import '../src/assets/scss/index.scss';
+// TODO add theme support
+import '../src/assets/scss/themes/_gk.scss';
+
+export const decorators = [
+  (story, params) => {
+    const rootElement = document.getElementById('storybook-root');
+
+    if (rootElement) {
+      // TODO add theme
+      const theme = 'gk';
+
+      rootElement.classList.add('shared-components');
+      rootElement.classList.add(theme);
+    }
+
+    return story();
+  },
+];
 
 /** @type { import('@storybook/vue3').Preview } */
 const preview = {
