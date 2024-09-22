e80c30510de6bfb22df2c998e6b6e092fcf022d9
Michael Ehrich <michael.ehrich@krekeny.com>
2024-09-22 16:57:21 +0200

    feat(headline): reverts headline back to js component

--- src/stories/components/headline.stories.js
+++ src/stories/components/headline.stories.js
@@ -1,51 +1,51 @@
-import HeadlineComponent from "../../components/Headline.vue";
+import HeadlineComponent from '../../components/Headline';
 
 export default {
-  title: "Components/Headline",
+  title: 'Components/Headline',
   component: HeadlineComponent,
 };
 
 export const h1 = {
   args: {
-    level: "h1",
-    text: "Headline Level 1",
+    level: 'h1',
+    text: 'Headline Level 1',
   },
 };
 
 export const h2 = {
   args: {
-    level: "h2",
-    text: "Headline Level 2",
+    level: 'h2',
+    text: 'Headline Level 2',
   },
 };
 
 export const h3 = {
   args: {
-    level: "h3",
-    text: "Headline Level 3",
+    level: 'h3',
+    text: 'Headline Level 3',
   },
 };
 
 export const h4 = {
   args: {
-    level: "h4",
-    text: "Headline Level 4",
+    level: 'h4',
+    text: 'Headline Level 4',
   },
 };
 
 export const h1WithH4Style = {
   args: {
-    level: "h1",
-    text: "Headline Level 1 with H4 Style",
-    classes: "h4-font-size",
+    level: 'h1',
+    text: 'Headline Level 1 with H4 Style',
+    classes: 'h4-font-size',
   },
 };
 
 export const h1WithFontSize3Style = {
   args: {
-    level: "h1",
-    text: "Headline Level 1 with Font Size 3 Style",
-    classes: "font-size-3",
+    level: 'h1',
+    text: 'Headline Level 1 with Font Size 3 Style',
+    classes: 'font-size-3',
   },
 };
 
