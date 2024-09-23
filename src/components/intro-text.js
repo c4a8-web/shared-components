05b42d927a9db734706ac757c2a5eed67127cd94
Michael Ehrich <michael.ehrich@krekeny.com>
2024-09-23 14:31:33 +0200

    feat(intro-text): migrates through script

--- src/components/intro-text.js
+++ src/components/intro-text.js
@@ -1,7 +1,30 @@
 export default {
   tagName: 'intro-text',
-  migrate: true,
+  props: {
+    copy: String,
+    variant: String,
+    headline: String,
+    spacing: String,
+    headlineClasses: {
+      type: String,
+      default: 'text-center',
+    },
+    copyClasses: String,
+  },
   template: `
-                        intro-text
-                    `,
+    <figure :class="['intro-text utility-animation', variant, spacing]">
+      <div class="container space-lg-2">
+        <div v-if="headline" class="row mb-9 mt-9">
+          <div :class="['col', headlineClasses, 'fade-in-bottom']" data-utility-animation-step="1">
+            <headline :text="headline" />
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <p :class="['intro-text__copy fade-in-bottom', copyClasses]" data-utility-animation-step="1" v-html="copy"></p>
+          </div>
+        </div>
+      </div>
+    </figure>
+  `,
 };
