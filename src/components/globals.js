42d9e4585ab2d9bb85b8b204b3eaa182f4af9769
Michael Ehrich <michael.ehrich@krekeny.com>
2024-09-23 16:25:44 +0200

    chore(globals): removes debug

--- src/components/globals.js
+++ src/components/globals.js
@@ -3,9 +3,7 @@ import { triggerVueMounted } from '../migrate';
 export default {
   tagName: 'globals',
   mounted() {
-    // onmounted send vue ready event
     triggerVueMounted();
-    console.log('vue mounted');
   },
   template: `
         <slot></slot>
