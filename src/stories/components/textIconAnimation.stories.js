9d0ad7d3e56f0df257317b40291672816dfb0c4f
Michael Ehrich <michael.ehrich@krekeny.com>
2024-09-23 14:38:07 +0200

    fix(text-icon-animation): resolves path

--- src/stories/components/textIconAnimation.stories.js
+++ src/stories/components/textIconAnimation.stories.js
@@ -1,9 +1,9 @@
-import ComponentName from "../../components/component-name";
+import TextIconComponent from '../../components/text-icon-animation';
 
 export default {
-  component: ComponentName,
+  component: TextIconComponent,
   argTypes: {},
-  title: "Components/Text Icon Animation",
+  title: 'Components/Text Icon Animation',
 };
 
 export const TextIconAnimation = {
@@ -11,25 +11,25 @@ export const TextIconAnimation = {
     animation: {
       sequence: [
         {
-          text: "HEY,",
-          textSize: "font-size-7",
+          text: 'HEY,',
+          textSize: 'font-size-7',
         },
         {
-          text: "Wo bin ich denn hier gelandet?",
+          text: 'Wo bin ich denn hier gelandet?',
         },
         {
-          text: "Da, wo eine neue Welt entsteht",
+          text: 'Da, wo eine neue Welt entsteht',
         },
         {
-          text: "Gemeinsam",
+          text: 'Gemeinsam',
           subline:
-            "wollen wir nichts weniger als die Welt verändern: die Dinge neu denken, die Revolution des Digitalen, einen Entwicklungssprung für unsere Kunden. Dafür braucht es Menschen, die nicht nach einem bloßen Job, sondern nach einer echten Aufgabe suchen. Menschen, die eine Vision haben. Menschen, die Utopien Realität werden lassen wollen.",
+            'wollen wir nichts weniger als die Welt verändern: die Dinge neu denken, die Revolution des Digitalen, einen Entwicklungssprung für unsere Kunden. Dafür braucht es Menschen, die nicht nach einem bloßen Job, sondern nach einer echten Aufgabe suchen. Menschen, die eine Vision haben. Menschen, die Utopien Realität werden lassen wollen.',
         },
       ],
     },
     cta: {
-      text: "Zu den Stellenanzeigen",
+      text: 'Zu den Stellenanzeigen',
     },
-    icon: "smile",
+    icon: 'smile',
   },
 };
