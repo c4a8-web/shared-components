2c0b0141fb90259442dd27c32556ccc65076d372
Michael Ehrich <michael.ehrich@krekeny.com>
2024-09-23 19:05:26 +0200

    feat(contact): migrates

--- src/stories/components/contact.stories.js
+++ src/stories/components/contact.stories.js
@@ -1,4 +1,5 @@
-import ContactComponent from '../../../components/contact';
+import ContactComponent from '../../components/contact';
+import Contact from '../../../.storybook/data/contact.js';
 
 export default {
   component: ContactComponent,
@@ -36,129 +37,138 @@ export default {
 
 export const Expanded = {
   args: {
+    contact: Contact.infos,
     light: true,
   },
 };
 
 export const Collapsed = {
   args: {
-    collapsed: true,
-    infos: {
-      person: {
-        image: 'people/people-sophie-luna.png',
-        cloudinary: true,
-        alt: 'Kontakt mit Sophie',
-        name: 'Hast du Fragen? Sprich uns gerne an!',
-        details: [
-          {
-            text: 'Lorem ipsum dolor & Sit amet lorem',
-            href: 'mailto:kontakt@glueckkanja.com',
-            icon: 'site/mail',
-          },
-          {
-            text: '+49 69 4005520',
-            href: 'tel:+49 69 4005520',
-            details: 'lorem ipsum dolor sit amet lore',
-            icon: 'site/phone',
-          },
-        ],
-      },
-      form: {
-        ctaText: 'Absenden',
-        cta: {
-          skin: 'primary is-light',
+    contact: {
+      ...Contact.infos,
+      ...{
+        person: {
+          image: 'people/people-sophie-luna.png',
+          cloudinary: true,
+          alt: 'Kontakt mit Sophie',
+          name: 'Hast du Fragen? Sprich uns gerne an!',
+          details: [
+            {
+              text: 'Lorem ipsum dolor & Sit amet lorem',
+              href: 'mailto:kontakt@glueckkanja.com',
+              icon: 'site/mail',
+            },
+            {
+              text: '+49 69 4005520',
+              href: 'tel:+49 69 4005520',
+              details: 'lorem ipsum dolor sit amet lore',
+              icon: 'site/phone',
+            },
+          ],
         },
-        method: 'post',
-        action: '/',
-        fields: [
-          {
-            label: 'Vorname',
-            type: 'text',
-            id: 'prename',
-            required: true,
-            requiredMsg: 'Bitte geben Sie Ihren Vornamen an.',
+        form: {
+          ctaText: 'Absenden',
+          cta: {
+            skin: 'primary is-light',
           },
-          {
-            label: 'Nachname',
-            type: 'text',
-            id: 'surname',
-            required: true,
-            requiredMsg: 'Bitte geben Sie Ihren Nachnamen an.',
-          },
-          {
-            label: 'Email-Adresse',
-            type: 'email',
-            id: 'email',
-            required: true,
-            requiredMsg: 'Bitte geben Sie Ihre E-Mail-Adresse ein.',
-          },
-          {
-            label: 'Nachricht',
-            type: 'textarea',
-            id: 'message',
-            required: false,
-            requiredMsg: 'Bitte ausfüllen',
-          },
-          {
-            label:
-              'Ich bin mit der Speicherung meiner Daten einverstanden, die Datenschutzerklärung habe ich gelesen und akzeptiert. Meine Einwilligung kann ich jederzeit widerrufen.',
-            type: 'checkbox',
-            id: 'checkbox1',
-          },
-          {
-            type: 'hidden',
-            id: '_to',
-            value: 'loremipsum@glueckkanja.com',
-          },
-          {
-            type: 'hidden',
-            id: '_gotcha',
-          },
-        ],
+          method: 'post',
+          action: '/',
+          fields: [
+            {
+              label: 'Vorname',
+              type: 'text',
+              id: 'prename',
+              required: true,
+              requiredMsg: 'Bitte geben Sie Ihren Vornamen an.',
+            },
+            {
+              label: 'Nachname',
+              type: 'text',
+              id: 'surname',
+              required: true,
+              requiredMsg: 'Bitte geben Sie Ihren Nachnamen an.',
+            },
+            {
+              label: 'Email-Adresse',
+              type: 'email',
+              id: 'email',
+              required: true,
+              requiredMsg: 'Bitte geben Sie Ihre E-Mail-Adresse ein.',
+            },
+            {
+              label: 'Nachricht',
+              type: 'textarea',
+              id: 'message',
+              required: false,
+              requiredMsg: 'Bitte ausfüllen',
+            },
+            {
+              label:
+                'Ich bin mit der Speicherung meiner Daten einverstanden, die Datenschutzerklärung habe ich gelesen und akzeptiert. Meine Einwilligung kann ich jederzeit widerrufen.',
+              type: 'checkbox',
+              id: 'checkbox1',
+            },
+            {
+              type: 'hidden',
+              id: '_to',
+              value: 'loremipsum@glueckkanja.com',
+            },
+            {
+              type: 'hidden',
+              id: '_gotcha',
+            },
+          ],
+        },
       },
     },
+    collapsed: true,
   },
 };
 
 export const WithShape = {
   args: {
-    infos: {
-      headline: 'What do you want to do next?',
-      level: 'h2',
-      person: {
-        image: '/people/people-sophie-luna.jpg',
-        cloudinary: true,
-        alt: 'Sophie Luna',
-        name: 'Sophie Luna',
-        mail: 'sales@RADIUSaaS.com',
+    contact: {
+      ...Contact.infos,
+      ...{
+        headline: 'What do you want to do next?',
+        level: 'h2',
+        person: {
+          image: '/people/people-sophie-luna.jpg',
+          cloudinary: true,
+          alt: 'Sophie Luna',
+          name: 'Sophie Luna',
+          mail: 'sales@RADIUSaaS.com',
+        },
+        hasGreyBackground: true,
+        svgShape: {
+          align: 'bottom',
+          peak: 'left',
+        },
+        buttons: [
+          {
+            ctaText: 'Try RADIUSaas',
+            ctaHref: 'start-now/#try',
+            skin: 'primary is-cutoff',
+          },
+          {
+            ctaText: 'Drop us a question',
+            ctaHref: 'drop-a-question',
+            skin: 'secondary is-cutoff',
+          },
+          {
+            ctaText: 'Get a quote',
+            ctaHref: 'start-now/#quote',
+            skin: 'secondary is-cutoff',
+          },
+          {
+            ctaText: 'Buy RADIUSaas',
+            ctaHref: 'start-now',
+            skin: 'secondary is-cutoff',
+          },
+        ],
+        form: null,
+        subline: null,
       },
-      hasGreyBackground: true,
-      svgShape: {
-        align: 'bottom',
-        peak: 'left',
-      },
-      buttons: [
-        {
-          ctaText: 'Try RADIUSaas',
-          ctaHref: 'start-now/#try',
-          skin: 'primary is-cutoff',
-        },
-        {
-          ctaText: 'Drop us a question',
-          ctaHref: 'drop-a-question',
-          skin: 'secondary is-cutoff',
-        },
-        {
-          ctaText: 'Get a quote',
-          ctaHref: 'start-now/#quote',
-          skin: 'secondary is-cutoff',
-        },
-        {
-          ctaText: 'Buy RADIUSaas',
-          ctaHref: 'start-now',
-          skin: 'secondary is-cutoff',
-        },
-      ],
     },
   },
 };
