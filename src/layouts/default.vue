a78768f0e7bda94aa70c7dca26b08237a4a6f787
Michael Ehrich <michael.ehrich@krekeny.com>
2024-09-25 14:04:09 +0200

    feat(default): passes lang to components

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="app">
-    <v-header v-bind="headerData"></v-header>
+    <v-header v-bind="headerData" :lang="lang"></v-header>
     <main class="page-content" aria-label="Content">
       main part
       <slot />
     </main>
     contact footer
 
-    <v-footer v-bind="footerData"></v-footer>
+    <v-footer v-bind="footerData" :lang="lang"></v-footer>
   </div>
 </template>
 
@@ -15,5 +15,9 @@
 const props = defineProps({
   headerData: Object,
   footerData: Object,
+  lang: {
+    type: String,
+    default: 'en',
+  },
 });
 </script>
