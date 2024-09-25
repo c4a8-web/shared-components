7aef099fca3b94c7dd2daec25589edb7eca36001
Michael Ehrich <michael.ehrich@krekeny.com>
2024-09-25 14:04:31 +0200

    chore(build): updates package export files

---
+++ dist/layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <v-header v-bind="headerData" :lang="lang"></v-header>
+    <main class="page-content" aria-label="Content">
+      main part
+      <slot />
+    </main>
+    contact footer
+
+    <v-footer v-bind="footerData" :lang="lang"></v-footer>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  headerData: Object,
+  footerData: Object,
+  lang: {
+    type: String,
+    default: 'en',
+  },
+});
+</script>
