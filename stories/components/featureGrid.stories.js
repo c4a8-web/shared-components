880bc05f1a6cfa8a0f23144c7c3a24599e98c294
Michael Ehrich <michael.ehrich@krekeny.com>
2024-12-16 12:51:52 +0100

    feat(featureGrid): adds story and adjusts styling

--- stories/components/featureGrid.stories.js
+++ stories/components/featureGrid.stories.js
@@ -230,3 +230,49 @@ export const WithAnimation = {
     },
   },
 };
+
+export const WithOverline = {
+  args: {
+    featureGrid: {
+      overline: 'This is an overline',
+      headline: 'Specifications',
+      features: [
+        {
+          title: 'Contact Sources',
+          image: {
+            src: 'svg/icons/unifiedcontacts_feature_contactsources.svg',
+            alt: 'Contact Sources',
+          },
+          list: ['Azure AD Users', 'Azure AD Contacts (coming soon)', 'Personal Exchange Online Contacts'],
+        },
+        {
+          title: 'Contact Card',
+          image: {
+            src: 'svg/icons/unifiedcontacts_feature_contactcards.svg',
+            alt: 'Contact Card',
+          },
+          list: ['Place PSTN calls', 'Place Teams calls', 'Write email', 'Start Teams chat', 'Get presence status'],
+        },
+        {
+          title: 'Platforms',
+          image: {
+            src: 'svg/icons/unifiedcontacts_feature_platforms.svg',
+            alt: 'Platforms',
+          },
+          list: ['Teams Desktop Client', 'Teams Web App', 'Teams Mobile App'],
+        },
+        {
+          title: 'ISO 27001',
+          image: {
+            src: 'svg/icons/unifiedcontacts_feature_certificates.svg',
+            alt: 'ISO 27001',
+          },
+          list: [
+            'Our development and operations teams are certified',
+            '<a href="https://www.datenschutz-cert.de/uploads/tx_dsnordreferenzen/glueckkanja_ISO27001_Urkunde_20211203_digital.pdf">ISO 27001 Certificate</a>',
+          ],
+        },
+      ],
+    },
+  },
+};
