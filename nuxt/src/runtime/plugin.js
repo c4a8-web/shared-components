561d209bde3f972670b64b94243bdc559dfd6bdb
Michael Ehrich <michael.ehrich@krekeny.com>
2024-12-23 19:31:30 +0100

    fix(v-menu): removes unwanted starting animation

--- nuxt/src/runtime/plugin.js
+++ nuxt/src/runtime/plugin.js
@@ -36,7 +36,7 @@ export default defineNuxtPlugin((_nuxtApp) => {
 
   if (process.client) {
     // TODO read this from package.json . could be an issue because of the moving and the relative location of the file
-    console.debug('Shared Components v.0.1.90');
+    console.debug('Shared Components v.0.1.91');
 
     import('jquery')
       .then((module) => {
