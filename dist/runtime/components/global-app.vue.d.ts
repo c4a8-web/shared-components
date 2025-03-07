4e9e7b0b5762406d23fda9c4576a6e2d67181693
Michael Ehrich <michael.ehrich@krekeny.com>
2025-03-07 15:06:21 +0100

    chore(package): updates to 0.1.111

--- dist/runtime/components/global-app.vue.d.ts
+++ dist/runtime/components/global-app.vue.d.ts
@@ -1,8 +1,16 @@
 declare namespace _default {
     let tagName: string;
     function mounted(): void;
+    namespace methods {
+        function captureBodyMutation(): void;
+        function handleBodyMutation(mutations: any): void;
+        function triggerResizeEvent(): void;
+    }
     namespace props {
         let classes: ArrayConstructor;
     }
+    function data(): {
+        lastBodyHeight: null;
+    };
 }
 export default _default;
--- dist/runtime/plugin.js
+++ dist/runtime/plugin.js
@@ -51,7 +51,7 @@ export default defineNuxtPlugin((_nuxtApp) => {
 
   if (process.client) {
     // TODO read this from package.json . could be an issue because of the moving and the relative location of the file
-    console.debug('Shared Components v.0.1.110');
+    console.debug('Shared Components v.0.1.111');
 
     import('jquery')
       .then((module) => {
